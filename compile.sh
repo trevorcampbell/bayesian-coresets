d3caa39219bb93ac9cc7ed50fd7fd41f684c5991
Trevor Campbell <tdjc@mit.edu>
2017-08-02 15:30:19 -0400

    removed unused files

--- compile.sh
+++
@@ -1,3 +0,0 @@
-g++ -std=c++14 -fPIC -shared -Og -g -Wno-ignored-attributes -Wno-misleading-indentation -Wno-deprecated-declarations -I/usr/local/include/eigen3/ src/ehkmeans.cpp -o ehkmeans/libehkmeans.so
-
-
--- setup.py
+++ setup.py
@@ -4,14 +4,12 @@ import numpy as np
 setup(
     name = 'hilbert-coresets',
     version='0.1',
-    description="Coresets in a Hilbert space",
+    description="Hilbert coresets for approximate Bayesian inference",
     author='Trevor Campbell',
     author_email='tdjc@mit.edu',
     url='https://github.com/trevorcampbell/hilbert-coresets/',
     packages=['hilbertcoresets'],
-    #package_data={'python' : ['*.so']},
     install_requires=['numpy'],
     keywords = ['coreset', 'hilbert', 'frank-wolfe'],
     platforms='ALL',
-    #scripts=['compile.sh'],
 )
