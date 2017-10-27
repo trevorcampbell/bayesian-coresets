ce4fc5da5f59a7eaf98699921e7d3f46dbc1c61e
Trevor Campbell <tdjc@mit.edu>
2017-10-27 15:56:33 -0400

    added fast/accurate method selection to fw/giga

--- hilbertcoresets/__init__.py
+++ hilbertcoresets/__init__.py
@@ -1,1 +1,5 @@
-from hilbertcoresets import ProjectedFrankWolfe, ProjectedImportanceSampling, FullDataset, RandomSubsample
+from frankwolfe import FrankWolfe
+from subsampling import ImportanceSampling, RandomSubsampling
+from fulldataset import FullDataset
+from projection import ProjectionF, Projection2
+from geodesicascent import GIGA
