made plots paper-friendly; added gaussian kl comparison; fixed bugs in bayes expts

examples/bayes/model_poiss.py
@@ -6,7 +6,7 @@ def load_data(dnm):
   Z = np.hstack((data['X'], data['y'][:, np.newaxis]))
   Zt = np.hstack((data['Xt'], data['yt'][:, np.newaxis]))
   data.close()
-  return Z, Zt
+  return Z, Zt, Z.shape[1]-1
 
 def gen_synthetic(n):
   X = np.random.randn(n)

examples/synth/main.py
@@ -2,18 +2,15 @@ import numpy as np
 import hilbertcoresets as hc
 import time
 
-n_trials = 3
-anms = ['GIGA(A)', 'GIGA(L)', 'GIGA(T)', 'FW', 'RND']
+n_trials = 50
+Ms = np.unique(np.logspace(0., 4., 100, dtype=np.int32))
+anms = ['GIGA', 'FW', 'RND']
 
 ##########################################
 ## Test 1: 1M 50-dimensional gaussian data
 ##########################################
-#N = 1000000
-#D = 50
-#Ms = np.linspace(1, 10000, 10000, dtype=np.int32)
-N = 10000
-D = 10
-Ms = np.linspace(1, 1000, 1000, dtype=np.int32)
+N = 1000000
+D = 50
 
 err = np.zeros((len(anms), n_trials, Ms.shape[0]))
 nfunc = np.zeros((len(anms), n_trials, Ms.shape[0]))
@@ -24,7 +21,7 @@ for tr in range(n_trials):
   for aidx, anm in enumerate(anms):
     print 'data: gauss, trial ' + str(tr+1) + '/' + str(n_trials) + ', alg: ' + anm
     alg = None
-    if 'GIGA' in anm:
+    if anm == 'GIGA':
       alg = hc.GIGA(X)
     elif anm == 'FW':
       alg = hc.FrankWolfe(X)
@@ -32,22 +29,9 @@ for tr in range(n_trials):
       alg = hc.RandomSubsampling(X) 
 
     for m, M in enumerate(Ms):
-      if anm == 'GIGA(L)':
-        t0 = time.clock()
-        alg.run(M, search_method='linear')
-        tf = time.clock()
-      elif anm == 'GIGA(T)':
-        t0 = time.clock()
-        alg.run(M, search_method='tree')
-        tf = time.clock()
-      elif anm == 'GIGA(A)':
-        t0 = time.clock()
-        alg.run(M, search_method='adaptive')
-        tf = time.clock()
-      else:
-        t0 = time.clock()
-        alg.run(M)
-        tf = time.clock()
+      t0 = time.clock()
+      alg.run(M)
+      tf = time.clock()
       cput[aidx, tr, m] = tf-t0 + cput[aidx, tr, m-1] if m > 0 else tf-t0
       wts = alg.weights()
       err[aidx, tr, m] = np.sqrt((((wts[:, np.newaxis]*X).sum(axis=0) - XS)**2).sum())
@@ -59,12 +43,7 @@ np.savez_compressed('gauss_results.npz', err=err, nfunc=nfunc, cput=cput, Ms = M
 ## Test 2: 5K axis-aligned data
 ##########################################
  
-#N = 5000
-#Ms = np.linspace(1, 10000, 10000, dtype=np.int32)
-
-N = 500
-Ms = np.linspace(1, 1000, 1000, dtype=np.int32)
-
+N = 5000
 
 X = np.eye(N)
 XS = np.ones(N)
@@ -76,7 +55,7 @@ for tr in range(n_trials):
   for aidx, anm in enumerate(anms):
     print 'data: axis, trial ' + str(tr+1) + '/' + str(n_trials) + ', alg: ' + anm
     alg = None
-    if 'GIGA' in anm:
+    if anm == 'GIGA':
       alg = hc.GIGA(X)
     elif anm == 'FW':
       alg = hc.FrankWolfe(X)
@@ -84,22 +63,9 @@ for tr in range(n_trials):
       alg = hc.RandomSubsampling(X) 
 
     for m, M in enumerate(Ms):
-      if anm == 'GIGA(L)':
-        t0 = time.clock()
-        alg.run(M, search_method='linear')
-        tf = time.clock()
-      elif anm == 'GIGA(T)':
-        t0 = time.clock()
-        alg.run(M, search_method='tree')
-        tf = time.clock()
-      elif anm == 'GIGA(A)':
-        t0 = time.clock()
-        alg.run(M, search_method='adaptive')
-        tf = time.clock()
-      else:
-        t0 = time.clock()
-        alg.run(M)
-        tf = time.clock()
+      t0 = time.clock()
+      alg.run(M)
+      tf = time.clock()
       cput[aidx, tr, m] = tf-t0 + cput[aidx, tr, m-1] if m > 0 else tf-t0
       wts = alg.weights()
       err[aidx, tr, m] = np.sqrt((((wts[:, np.newaxis]*X).sum(axis=0) - XS)**2).sum())

examples/synth/plot.py
@@ -32,18 +32,28 @@ if (tick_mult > 1.) {
 ret += '10';
 if (tick_power < 0){
   ret += '\u207B';
+  tick_power = -tick_power;
 }
-ret += trns[Math.abs(tick_power)];
+power_digits = []
+while (tick_power > 9){
+  power_digits.push( tick_power - Math.floor(tick_power/10)*10 )
+  tick_power = Math.floor(tick_power/10)
+}
+power_digits.push(tick_power)
+for (i = power_digits.length-1; i >= 0; i--){
+  ret += trns[power_digits[i]];
+}
 return ret;
 """)
 
 
-fig_err_g = bkp.figure(y_axis_type='log', x_axis_type='log', y_axis_label='Err', x_axis_label='M', plot_width=1250, plot_height=1250)
-fig_cost_g = bkp.figure(y_axis_type='log', x_axis_type='log', y_axis_label='Time (ms) & #Ops', x_axis_label='M', plot_width=1250, plot_height=1250)
-fig_err_a = bkp.figure(y_axis_type='log', x_axis_type='log', y_axis_label='Err', x_axis_label='M', plot_width=1250, plot_height=1250)
-fig_cost_a = bkp.figure(y_axis_type='log', x_axis_type='log', y_axis_label='Time (ms) & #Ops', x_axis_label='M', plot_width=1250, plot_height=1250)
+fig_err_g = bkp.figure(y_axis_type='log', x_axis_type='log', y_axis_label='Error', x_axis_label='Coreset Construction Iterations', plot_width=1250, plot_height=1250)
+fig_cost_g = bkp.figure(y_axis_type='log', x_axis_type='log', y_axis_label='Time (s)', x_axis_label='Coreset Construction Iterations', plot_width=1250, plot_height=1250)
+fig_err_a = bkp.figure(y_axis_type='log', x_axis_type='log', y_axis_label='Error', x_axis_label='Coreset Construction Iterations', plot_width=1250, plot_height=1250)
+fig_cost_a = bkp.figure(y_axis_type='log', x_axis_type='log', y_axis_label='Time (s)', x_axis_label='Coreset Construction Iterations', plot_width=1250, plot_height=1250)
 
-axis_font_size='30pt'
+axis_font_size='36pt'
+legend_font_size='36pt'
 for f in [fig_err_g, fig_cost_g, fig_err_a, fig_cost_a]:
   #f.xaxis.ticker = bkm.tickers.FixedTicker(ticks=[.1, 1])
   f.xaxis.axis_label_text_font_size= axis_font_size
@@ -60,13 +70,12 @@ gr = np.load('gauss_results.npz')
 anms = gr['anms']
 Ms = gr['Ms']
 err = gr['err']
-nfunc = gr['nfunc']
 cput = gr['cput']
-pal = bokeh.palettes.colorblind['Colorblind'][len(anms)]
+pal = bokeh.palettes.colorblind['Colorblind'][8]
+pal = [pal[0], pal[1], pal[3], pal[4], pal[5], pal[6], pal[7], pal[2]]
 for aidx, anm in enumerate(anms):
-  fig_err_g.line(Ms, np.percentile(err[aidx,:,:], 50, axis=0), line_color=pal[aidx], line_width=4, legend=anm)
-  fig_cost_g.line(Ms, np.percentile(nfunc[aidx,:,:], 50, axis=0), line_color=pal[aidx], line_width=4, legend=anm)
-  fig_cost_g.line(Ms, np.percentile(cput[aidx,:,:], 50, axis=0), line_color=pal[aidx], line_width=4, line_dash='dashed')
+  fig_err_g.line(Ms, np.percentile(err[aidx,:,:], 50, axis=0), line_color=pal[aidx], line_width=8, legend=anm)
+  fig_cost_g.line(Ms, np.percentile(cput[aidx,:,:], 50, axis=0), line_color=pal[aidx], line_width=8, legend=anm)
 
 
 aa = np.load('axis_results.npz')
@@ -77,13 +86,12 @@ nfunc = aa['nfunc']
 cput = aa['cput']
 pal = bokeh.palettes.colorblind['Colorblind'][len(anms)]
 for aidx, anm in enumerate(anms):
-  fig_err_a.line(Ms, np.percentile(err[aidx,:,:], 50, axis=0), line_color=pal[aidx], line_width=4, legend=anm)
-  fig_cost_a.line(Ms, np.percentile(nfunc[aidx,:,:], 50, axis=0), line_color=pal[aidx], line_width=4, legend=anm)
-  fig_cost_a.line(Ms, np.percentile(cput[aidx,:,:], 50, axis=0), line_color=pal[aidx], line_width=4, line_dash='dashed')
+  fig_err_a.line(Ms, np.percentile(err[aidx,:,:], 50, axis=0), line_color=pal[aidx], line_width=8, legend=anm)
+  fig_cost_a.line(Ms, np.percentile(cput[aidx,:,:], 50, axis=0), line_color=pal[aidx], line_width=8, legend=anm)
 
  
 for f in [fig_err_g, fig_cost_g, fig_err_a, fig_cost_a]:
-  f.legend.label_text_font_size= '16pt'
+  f.legend.label_text_font_size= legend_font_size
   f.legend.glyph_width=40
   f.legend.glyph_height=40
   f.legend.spacing=20

hilbertcoresets/geodesicascent.py
@@ -76,7 +76,7 @@ class GIGA(object):
     #  self.M = 1
 
     for m in range(self.M, M):
-      f = self.search(max_approx_search_evals)
+      f = self.search()
       gA = -1.
       gB = -1.
       if f >= 0:
@@ -94,7 +94,7 @@ class GIGA(object):
         self.f_update += (self.wts > 0).sum() + 3.
 
         #run an exact linear search here
-        f = self.search_linear(max_approx_search_evals)
+        f = self.search_linear()
         if f >= 0:
           gA = self.ys.dot(self.y[f,:]) - self.ys.dot(self.yw) * self.yw.dot(self.y[f,:])
           gB = self.ys.dot(self.yw) - self.ys.dot(self.y[f,:]) * self.yw.dot(self.y[f,:])
@@ -144,7 +144,7 @@ class GIGA(object):
   def search_tree(self, max_evals):
 
     if not self.tree.is_build_done():
-      return self.search_linear(max_evals)
+      return self.search_linear()
 
     cdir = self.ys - self.ys.dot(self.yw)*self.yw
     cdirnrm =np.sqrt((cdir**2).sum()) 