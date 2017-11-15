made plots paper-friendly; added gaussian kl comparison; fixed bugs in bayes expts

examples/bayes/main.py
@@ -1,66 +1,79 @@
 import numpy as np
-from inference import mh, wasserstein1
+from inference import mh, wasserstein1, gaussian_KL
 import hilbertcoresets as hc
 from scipy.optimize import minimize
 import time
 
 
-## FOR LOGISTIC REGRESSION
+# FOR LOGISTIC REGRESSION
 from model_lr import *
 dnames = ['synth', 'ds1', 'phishing']
 fldr = 'lr'
 
-### FOR POISSON REGRESSION
+#### FOR POISSON REGRESSION
 #from model_poiss import *
 #dnames = ['synth', 'airportdelays', 'biketrips']
-#folder = 'poiss'
+#fldr = 'poiss'
 
 
-#mh_steps = 100000 #total number of MH steps
-#mh_thin = 5 #thinning factor
-#mh_target = 0.234 #target acceptance rate
-#mh_step_var_init = 0.1 #initial step variance
-#n_samples = mh_steps / 2 / mh_thin #number of output samples (burn of 1/2)
-#projection_dim = 500 #random projection dimension
-#Ms = np.array([10, 50, 100, 500, 1000, 5000, 10000]) #values of M to sweep over
-#anms = ['GIGA', 'FW', 'RND']
-#n_trials = 10
-
-
-#Fast test params
-mh_steps = 10000 #total number of MH steps
+mh_steps = 100000 #total number of MH steps
 mh_thin = 5 #thinning factor
 mh_target = 0.234 #target acceptance rate
 mh_step_var_init = 0.1 #initial step variance
 n_samples = mh_steps / 2 / mh_thin #number of output samples (burn of 1/2)
-projection_dim = 200 #random projection dimension
-Ms = np.array([10, 50, 100, 500, 1000]) #values of M to sweep over
+projection_dim = 500 #random projection dimension
+Ms = np.array([10, 50, 100, 500, 1000, 5000, 10000]) #values of M to sweep over
 anms = ['GIGA', 'FW', 'RND']
 n_trials = 10
 
 
+##Fast test params
+#mh_steps = 1000 #total number of MH steps
+#mh_thin = 5 #thinning factor
+#mh_target = 0.234 #target acceptance rate
+#mh_step_var_init = 0.1 #initial step variance
+#n_samples = mh_steps / 2 / mh_thin #number of output samples (burn of 1/2)
+#projection_dim = 100 #random projection dimension
+#Ms = np.array([10, 50, 100, 500, 1000]) #values of M to sweep over
+#anms = ['GIGA', 'FW', 'RND']
+#n_trials = 3
+
 
 for dnm in dnames:
   print 'Loading dataset '+dnm
-  Z, Zt = load_data(fldr+'/'+dnm+'.npz')
-  D = Z.shape[1]
+  Z, Zt, D = load_data(fldr+'/'+dnm+'.npz')
 
   print 'Computing Laplace approximation'
   t0 = time.clock()
-  res = minimize(lambda mu : -log_joint(Z, mu, np.ones(Z.shape[0])), Z.mean(axis=0), jac=lambda mu : -grad_log_likelihood(Z, mu).sum(axis=0) - grad_log_prior(mu))
+  res = minimize(lambda mu : -log_joint(Z, mu, np.ones(Z.shape[0])), Z.mean(axis=0)[:D], jac=lambda mu : -grad_log_likelihood(Z, mu).sum(axis=0) - grad_log_prior(mu))
   mu = res.x
   cov = -np.linalg.inv(hess_log_joint(Z, mu))
   t_laplace = time.clock() - t0
 
   print 'Computing Max TestLL'
-  res = minimize(lambda mu : -log_joint(Zt, mu, np.ones(Zt.shape[0])), Zt.mean(axis=0), jac=lambda mu : -grad_log_likelihood(Zt, mu).sum(axis=0) - grad_log_prior(mu))
+  res = minimize(lambda mu : -log_joint(Zt, mu, np.ones(Zt.shape[0])), Zt.mean(axis=0)[:D], jac=lambda mu : -grad_log_likelihood(Zt, mu).sum(axis=0) - grad_log_prior(mu))
   ll_max = res.fun/Zt.shape[0]
 
   w1s = np.zeros((len(anms), n_trials, Ms.shape[0]))
   lls = np.zeros((len(anms), n_trials, Ms.shape[0]))
   cputs = np.zeros((len(anms), n_trials, Ms.shape[0]))
-  ll_fulls = np.zeros(n_trials)
-  cput_fulls = np.zeros(n_trials)
+  kls = np.zeros((len(anms), n_trials, Ms.shape[0]))
+  lls_full = np.zeros(n_trials)
+  cputs_full = np.zeros(n_trials)
+  w1s_full = np.zeros(n_trials)
+  kls_full = np.zeros(n_trials)
+
+  print 'Running MCMC on the full dataset'
+  mh_param_init = np.random.multivariate_normal(np.zeros(D), np.eye(D))
+  th_samples, accept_rate = mh(mh_param_init,
+         lambda th : log_joint(Z, th, np.ones(Z.shape[0])),
+         None,
+         lambda th, sig : np.random.multivariate_normal(th, sig*np.eye(D)),
+         steps=mh_steps,
+         thin=mh_thin,
+         target_rate=mh_target,
+         proposal_param=mh_step_var_init)
+  full_samples = np.array(th_samples)
 
   for tr in range(n_trials):
     print 'Trial ' + str(tr+1) +'/' + str(n_trials)
@@ -73,7 +86,6 @@ for dnm in dnames:
 
     print 'Running MCMC on the full dataset'
     t0 = time.clock()
-    full_alg = hc.FullDataset(vecs)
     mh_param_init = np.random.multivariate_normal(np.zeros(D), np.eye(D))
     th_samples, accept_rate = mh(mh_param_init,
            lambda th : log_joint(Z, th, np.ones(Z.shape[0])),
@@ -83,11 +95,11 @@ for dnm in dnames:
            thin=mh_thin,
            target_rate=mh_target,
            proposal_param=mh_step_var_init)
-    t_full_mh = time.clock()-t0
-    full_samples = np.array(th_samples)
-    ll_full = np.array([ log_likelihood(Zt, full_samples[i, :]).sum()/Zt.shape[0] for i in range(full_samples.shape[0]) ]).sum()/full_samples.shape[0]
-    ll_fulls[tr] = ll_full
-    cput_fulls[tr] = t_full_mh
+    cputs_full[tr] = time.clock()-t0
+    th_samples = np.array(th_samples)
+    lls_full[tr] = np.array([ log_likelihood(Zt, th_samples[i, :]).sum()/Zt.shape[0] for i in range(th_samples.shape[0]) ]).sum()/th_samples.shape[0]
+    w1s_full[tr] = wasserstein1(th_samples, full_samples)
+    kls_full[tr] = gaussian_KL(full_samples, th_samples)
     
     
     print 'Running coreset construction / MCMC'
@@ -128,15 +140,13 @@ for dnm in dnames:
         t_alg_mh = time.clock()-t0    
         th_samples = np.array(th_samples)
         print 'M = ' + str(Ms[m]) + ': 1-wasserstein'
-        w1 = wasserstein1(th_samples, full_samples)
+        w1s[aidx, tr, m] = wasserstein1(th_samples, full_samples)
         print 'M = ' + str(Ms[m]) + ': test log-likelihood'
-        ll = np.array([ log_likelihood(Zt, th_samples[i, :]).sum()/Zt.shape[0] for i in range(th_samples.shape[0]) ]).sum()/th_samples.shape[0]
-        print 'W1 = ' + str(w1)
-        print 'LL = ' + str(ll)
-        w1s[aidx, tr, m] = w1
-        lls[aidx, tr, m] = ll
+        lls[aidx, tr, m] = np.array([ log_likelihood(Zt, th_samples[i, :]).sum()/Zt.shape[0] for i in range(th_samples.shape[0]) ]).sum()/th_samples.shape[0]
+        print 'M = ' + str(Ms[m]) + ': Gaussian KL'
+        kls[aidx, tr, m] = gaussian_KL(full_samples, th_samples)
         cputs[aidx, tr, m] = t_laplace + t_projection + t_setup + t_alg + t_alg_mh
 
-  np.savez_compressed(fldr+'/'+dnm+'_results.npz', w1s=w1s, lls=lls, cputs=cputs, ll_fulls=ll_fulls, ll_max=ll_max, anms=anms, cput_fulls=cput_fulls)
+  np.savez_compressed(fldr+'/'+dnm+'_results.npz', w1s=w1s, lls=lls, kls=kls, cputs=cputs, lls_full=lls_full, cputs_full=cputs_full, w1s_full=w1s_full, kls_full=kls_full, ll_max=ll_max, anms=anms)
         
 

examples/bayes/model_lr.py
@@ -5,7 +5,7 @@ def load_data(dnm):
   Z = data['y'][:, np.newaxis]*data['X']
   Zt = data['yt'][:, np.newaxis]*data['Xt']
   data.close()
-  return Z, Zt
+  return Z, Zt, Z.shape[1]
 
 def gen_synthetic(n):
   mu = np.array([0, 0])

examples/bayes/plot.py
@@ -32,20 +32,37 @@ if (tick_mult > 1.) {
 ret += '10';
 if (tick_power < 0){
   ret += '\u207B';
+  tick_power = -tick_power;
 }
-ret += trns[Math.abs(tick_power)];
+power_digits = []
+while (tick_power > 9){
+  power_digits.push( tick_power - Math.floor(tick_power/10)*10 )
+  tick_power = Math.floor(tick_power/10)
+}
+power_digits.push(tick_power)
+for (i = power_digits.length-1; i >= 0; i--){
+  ret += trns[power_digits[i]];
+}
 return ret;
 """)
 
-dnames = ['synth', 'ds1', 'phishing']
-fldr = 'lr'
+
+#dnames = ['synth', 'ds1', 'phishing']
+#fldr = 'lr'
+
+dnames = ['synth', 'biketrips', 'airportdelays']
+fldr = 'poiss'
 
 
-fig_ll = bkp.figure(y_axis_type='log', x_axis_type='log', y_axis_label='Negative Test Log-Likelihood', x_axis_label='M', plot_width=1250, plot_height=1250)
-fig_w1 = bkp.figure(y_axis_type='log', x_axis_type='log', y_axis_label='1-Wasserstein', x_axis_label='M', plot_width=1250, plot_height=1250)
 
-axis_font_size='30pt'
-for f in [fig_ll, fig_w1]:
+
+fig_ll = bkp.figure(y_axis_type='log', x_axis_type='log', y_axis_label='Negative Test Log-Likelihood', x_axis_label='Coreset Construction Iterations', plot_width=1250, plot_height=1250)
+fig_w1 = bkp.figure(y_axis_type='log', x_axis_type='log', y_axis_label='1-Wasserstein', x_axis_label='Coreset Construction Iterations', plot_width=1250, plot_height=1250)
+fig_kl = bkp.figure(y_axis_type='log', x_axis_type='log', y_axis_label='Gaussian KL-Divergence', x_axis_label='Coreset Construction Iterations', plot_width=1250, plot_height=1250)
+
+axis_font_size='36pt'
+legend_font_size='36pt'
+for f in [fig_ll, fig_w1, fig_kl]:
   #f.xaxis.ticker = bkm.tickers.FixedTicker(ticks=[.1, 1])
   f.xaxis.axis_label_text_font_size= axis_font_size
   f.xaxis.major_label_text_font_size= axis_font_size
@@ -56,16 +73,20 @@ for f in [fig_ll, fig_w1]:
   f.toolbar.logo = None
   f.toolbar_location = None
 
-pal = bokeh.palettes.colorblind['Colorblind'][len(dnames)]
+pal = bokeh.palettes.colorblind['Colorblind'][8]
+pal = [pal[0], pal[1], pal[3], pal[4], pal[5], pal[6], pal[7], pal[2]]
 for didx, dnm in enumerate(dnames):
   
   res = np.load(fldr +'/' + dnm  + '_results.npz')
 
   w1s = res['w1s']
   lls = res['lls']
+  kls = res['kls']
   cputs = res['cputs']
-  ll_fulls = res['ll_fulls']
-  cput_fulls = res['cput_fulls']
+  w1s_full = res['w1s_full']
+  kls_full = res['kls_full']
+  lls_full = res['lls_full']
+  cputs_full = res['cputs_full']
   ll_max = res['ll_max']
   anms = res['anms']
 
@@ -77,15 +98,16 @@ for didx, dnm in enumerate(dnames):
     else:
       ld = 'solid'
     #TODO: make this relative to full vs cput, and relative to RND vs M 
-    fig_w1.line(np.percentile(cputs[aidx,:,:], 50, axis=0), np.percentile(w1s[aidx, :, :], 50, axis=0), line_color=pal[didx], line_width=4, line_dash=ld, legend=dnm if ld == 'solid' else None)
-    fig_ll.line(np.percentile(cputs[aidx,:,:], 50, axis=0), np.percentile(ll_max - lls[aidx, :, :], 50, axis=0), line_color=pal[didx], line_width=4, line_dash=ld, legend=dnm if ld == 'solid' else None)
+    fig_w1.line(np.percentile(cputs[aidx,:,:], 50, axis=0), np.percentile(w1s[aidx, :, :], 50, axis=0), line_color=pal[didx], line_width=8, line_dash=ld, legend=dnm if ld == 'solid' else None)
+    fig_ll.line(np.percentile(cputs[aidx,:,:], 50, axis=0), np.percentile(ll_max - lls[aidx, :, :], 50, axis=0), line_color=pal[didx], line_width=8, line_dash=ld, legend=dnm if ld == 'solid' else None)
+    fig_kl.line(np.percentile(cputs[aidx,:,:], 50, axis=0), np.percentile(kls[aidx, :, :], 50, axis=0), line_color=pal[didx], line_width=8, line_dash=ld, legend=dnm if ld == 'solid' else None)
   
-for f in [fig_ll, fig_w1]:
-  f.legend.label_text_font_size= '16pt'
+for f in [fig_ll, fig_w1, fig_kl]:
+  f.legend.label_text_font_size= legend_font_size
   f.legend.glyph_width=40
   f.legend.glyph_height=40
   f.legend.spacing=20
 
-bkp.show(bkl.gridplot([[fig_ll, fig_w1]]))
+bkp.show(bkl.gridplot([[fig_ll, fig_w1, fig_kl]]))
 
 