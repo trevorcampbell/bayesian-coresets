6d59b0b760f1af19ed22b855896fb94a14d35340
Trevor Campbell <tdjc@mit.edu>
2018-04-28 17:31:34 -0400

    added new imports to initpy

--- bayesiancoresets/__init__.py
+++ bayesiancoresets/__init__.py
@@ -3,3 +3,10 @@ from .subsampling import ImportanceSampling, RandomSubsampling
 from .fulldataset import FullDataset
 from .projection import ProjectionF, Projection2
 from .geodesicascent import GIGA
+
+#new imports
+from .lineargreedy import FrankWolfe2, Pursuit, ForwardStagewise
+from .geodesicascent import GIGA2
+from .subsampling import ImportanceSampling2, RandomSubsampling2
+from .orthopursuit import OrthoPursuit2
+from .lar import LAR
