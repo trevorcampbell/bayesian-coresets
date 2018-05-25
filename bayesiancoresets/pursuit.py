9c4e23d5d8169a90aea4ec9e85b2cf7fc44ed65a
Trevor Campbell <tdjc@mit.edu>
2018-05-24 20:37:30 -0400

    bugfix in pursuit step coeffs; import bugfix in subsampling; removed compilation of gigasearch

--- bayesiancoresets/pursuit.py
+++ bayesiancoresets/pursuit.py
@@ -8,9 +8,23 @@ class Pursuit(GreedySingleUpdate):
     return (((self.snorm*self.xs - self.xw)*self.x).sum(axis=1)).argmax()
 
   def _step_coeffs(self, f):
-    beta = (self.x[f, :]).dot(self.snorm*self.xs - self.xw)
-    if beta < 0.:
+    v1 = self.xw - self.xw.dot(self.x[f, :])*self.x[f, :]
+    v2 = (self.xw**2).sum()*self.x[f, :] - self.xw.dot(self.x[f, :])*self.xw
+ 
+    if (v1**2).sum() == 0.:
       return None, None
-    return 1.0, beta
+    
+    alpha = (self.snorm*self.xs).dot(v1) / self.xw.dot(v1)
+    beta = (self.snorm*self.xs).dot(v2) / self.xw.dot(v1)
+
+    if beta < 0. or alpha < 0.:
+      return None, None
+    return alpha,  beta
+
+    #old code; does not converge (e.g. try data [[-.1, .9], [.1, .9]] )
+    #beta = (self.x[f, :]).dot(self.snorm*self.xs - self.xw)
+    #if beta < 0.:
+    #  return None, None
+    #return 1.0, beta
 
 
--- setup.py
+++ setup.py
@@ -1,14 +1,4 @@
 from setuptools import setup
-from setuptools.command.install import install
-import subprocess
-
-
-class build_so(install):
-  def run(self):
-    retcode = subprocess.call(['g++', '-std=c++11', '-pthread', '-DNDEBUG', '-fwrapv', '-Ofast', '-fopenmp', '-Werror', '-Wall', '-Wextra', '-fno-strict-aliasing', '-D_FORTIFY_SOURCE=2', '-fstack-protector-strong', '-Werror=format-security', '-fPIC', 'bayesiancoresets/gigasearch.cpp', '-shared', '-o', 'bayesiancoresets/libgigasearch.so']) 
-    if retcode != 0:
-      raise Exception('g++: Compile of gigasearch.cpp failed')
-    return install.run(self)
 
 setup(
     name = 'bayesiancoresets',
@@ -21,6 +11,4 @@ setup(
     install_requires=['numpy', 'scipy'],
     keywords = ['Bayesian', 'inference', 'coreset', 'Hilbert', 'Frank-Wolfe', 'greedy', 'geodesic'],
     platforms='ALL',
-    cmdclass={'install':build_so},
-    package_data={'bayesiancoresets': ['libgigasearch.so']}
 )
