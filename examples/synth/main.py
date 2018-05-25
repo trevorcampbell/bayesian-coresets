31002fb93e61f0c0261decaca671fb7bedfb07a1
Trevor Campbell <tdjc@mit.edu>
2018-05-24 21:01:04 -0400

    updated synth example to use the new algs

--- examples/synth/main.py
+++ examples/synth/main.py
@@ -5,17 +5,19 @@ import time
 
 n_trials = 20
 Ms = np.unique(np.logspace(0., 4., 100, dtype=np.int32))
-anms = ['GIGA', 'FW', 'RND']
+anms = ['GIGA', 'FW', 'MP', 'FSW', 'OMP', 'LAR', 'IS', 'RND']
+algs = [bc.GIGA, bc.FrankWolfe, bc.ReweightedPursuit, bc.ForwardStagewise, bc.OrthoPursuit, bc.LAR, bc.ImportanceSampling, bc.RandomSubsampling]
+
 
 ##########################################
 ## Test 1: gaussian data
 ##########################################
 N = 1000000
+N = 10000
 D = 50
 
-
-
 err = np.zeros((len(anms), n_trials, Ms.shape[0]))
+scaled_err = np.zeros((len(anms), n_trials, Ms.shape[0]))
 csize = np.zeros((len(anms), n_trials, Ms.shape[0]))
 cput = np.zeros((len(anms), n_trials, Ms.shape[0]))
 for tr in range(n_trials):
@@ -23,13 +25,7 @@ for tr in range(n_trials):
   XS = X.sum(axis=0)
   for aidx, anm in enumerate(anms):
     print('data: gauss, trial ' + str(tr+1) + '/' + str(n_trials) + ', alg: ' + anm)
-    alg = None
-    if anm == 'GIGA':
-      alg = bc.GIGA(X)
-    elif anm == 'FW':
-      alg = bc.FrankWolfe(X)
-    else:
-      alg = bc.RandomSubsampling(X) 
+    alg = algs[aidx](X)
 
     for m, M in enumerate(Ms):
       t0 = time.time()
@@ -38,32 +34,30 @@ for tr in range(n_trials):
       cput[aidx, tr, m] = tf-t0 + cput[aidx, tr, m-1] if m > 0 else tf-t0
       wts = alg.weights()
       err[aidx, tr, m] = np.sqrt((((wts[:, np.newaxis]*X).sum(axis=0) - XS)**2).sum())
+      wts = alg.weights(optimal_scaling=True)
+      scaled_err[aidx, tr, m] = np.sqrt((((wts[:, np.newaxis]*X).sum(axis=0) - XS)**2).sum())
       csize[aidx, tr, m] = (wts > 0).sum()
 
-np.savez_compressed('gauss_results.npz', err=err, csize=csize, cput=cput, Ms = Ms, anms=anms)
+np.savez_compressed('gauss_results.npz', err=err, csize=csize, cput=cput, scaled_err=scaled_err, Ms = Ms, anms=anms)
 
 ##########################################
 ## Test 2: axis-aligned data
 ##########################################
  
 N = 5000
+N = 100
 
 X = np.eye(N)
 XS = np.ones(N)
 
 err = np.zeros((len(anms), n_trials, Ms.shape[0]))
+scaled_err = np.zeros((len(anms), n_trials, Ms.shape[0]))
 csize = np.zeros((len(anms), n_trials, Ms.shape[0]))
 cput = np.zeros((len(anms), n_trials, Ms.shape[0]))
 for tr in range(n_trials):
   for aidx, anm in enumerate(anms):
     print('data: axis, trial ' + str(tr+1) + '/' + str(n_trials) + ', alg: ' + anm)
-    alg = None
-    if anm == 'GIGA':
-      alg = bc.GIGA(X)
-    elif anm == 'FW':
-      alg = bc.FrankWolfe(X)
-    else:
-      alg = bc.RandomSubsampling(X) 
+    alg = algs[aidx](X)
 
     for m, M in enumerate(Ms):
       t0 = time.time()
@@ -72,7 +66,9 @@ for tr in range(n_trials):
       cput[aidx, tr, m] = tf-t0 + cput[aidx, tr, m-1] if m > 0 else tf-t0
       wts = alg.weights()
       err[aidx, tr, m] = np.sqrt((((wts[:, np.newaxis]*X).sum(axis=0) - XS)**2).sum())
+      wts = alg.weights(optimal_scaling=True)
+      scaled_err[aidx, tr, m] = np.sqrt((((wts[:, np.newaxis]*X).sum(axis=0) - XS)**2).sum())
       csize[aidx, tr, m] = (wts>0).sum()
 
-np.savez_compressed('axis_results.npz', err=err, csize=csize, cput=cput, Ms = Ms, anms=anms)
+np.savez_compressed('axis_results.npz', err=err, csize=csize, cput=cput, scaled_err=scaled_err, Ms = Ms, anms=anms)
 
