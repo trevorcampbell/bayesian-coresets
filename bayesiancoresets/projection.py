4eea58885f5f0224e449db5d5a62109d2fcb6522
Trevor Campbell <trevor.d.campbell@gmail.com>
2018-05-26 11:52:29 -0700

    removed dependence of _Projection on data/N

--- bayesiancoresets/projection.py
+++ bayesiancoresets/projection.py
@@ -1,10 +1,9 @@
 import numpy as np
 
 class _Projection(object):
-  def __init__(self, projection_dim, sample_approx_posterior):
-    self.N = self.data.shape[0]
+  def __init__(self, N, projection_dim, sample_approx_posterior):
     self.dim = sample_approx_posterior().shape[0]
-    self.x = np.zeros((self.N, 0))
+    self.x = np.zeros((N, 0))
     self.sample_approx_posterior = sample_approx_posterior
     self.update_projection_dimension(projection_dim)
     return
@@ -15,7 +14,7 @@ class _Projection(object):
 
     if projection_dim > self.x.shape[1]:
       old_dim = self.x.shape[1]
-      w = np.zeros((self.N, projection_dim))
+      w = np.zeros((self.x.shape[0], projection_dim))
       w[:, :old_dim] = self.x
       w *= np.sqrt(old_dim)
       for j in range(projection_dim-old_dim):
@@ -27,7 +26,6 @@ class _Projection(object):
   def reset_projection(self, projection_dim=None):
     if not projection_dim:
       projection_dim = self.x.shape[1]
-
     self.update_projection_dimension(0)
     self.update_projection_dimension(projection_dim)
     return
@@ -39,7 +37,7 @@ class Projection2(_Projection):
   def __init__(self, data, log_likelihood, projection_dim, sample_approx_posterior):
     self.data = data
     self.log_likelihood = log_likelihood
-    _Projection.__init__(self, projection_dim, sample_approx_posterior)
+    _Projection.__init__(self, data.shape[0], projection_dim, sample_approx_posterior)
 
   def sample_projection_component(self):
     return self.log_likelihood(self.data, self.sample_approx_posterior())
@@ -48,7 +46,7 @@ class ProjectionF(_Projection):
   def __init__(self, data, grad_log_likelihood, projection_dim, sample_approx_posterior):
     self.data = data
     self.grad_log_likelihood = grad_log_likelihood
-    _Projection.__init__(self, projection_dim, sample_approx_posterior)
+    _Projection.__init__(self, data.shape[0], projection_dim, sample_approx_posterior)
   
   def sample_projection_component(self):
     return np.sqrt(self.dim)*self.grad_log_likelihood(self.data, self.sample_approx_posterior(), np.random.randint(self.dim))
