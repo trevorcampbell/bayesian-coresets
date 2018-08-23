476bc7b281213e9386e6037b99da00223ac87757
Trevor Campbell <trevor.d.campbell@gmail.com>
2018-08-23 12:20:41 -0700

    updated tests for new mp

--- tests/test_opt.py
+++ tests/test_opt.py
@@ -10,8 +10,8 @@ tol = 1e-9
 
 
 n_trials = 10
-anms = ['GIGA', 'FW', 'RP', 'FSW', 'OMP', 'LAR']
-algs = [bc.GIGA, bc.FrankWolfe, bc.ReweightedPursuit, bc.ForwardStagewise, bc.OrthoPursuit, bc.LAR]
+anms = ['GIGA', 'FW', 'MP', 'FSW', 'OMP', 'LAR']
+algs = [bc.GIGA, bc.FrankWolfe, bc.MatchingPursuit, bc.ForwardStagewise, bc.OrthoPursuit, bc.LAR]
 algs_nms = zip(anms, algs)
 tests = [(N, D, dist, algn) for N in [1, 10, 100] for D in [1, 3, 10] for dist in ['gauss', 'bin', 'gauss_colinear', 'bin_colinear', 'axis_aligned'] for algn in algs_nms]
 
