e948d3c26f1e31cb4cbd89661f3d669469c176d2
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-04-07 16:52:20 -0700

    recoded base tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -9,26 +9,19 @@ np.random.seed(100)
 tol = 1e-9
 
 n_trials = 10
-anms = ['Full', 'IS', 'Uniform']
-algs = [bc.FullDataCoreset, bc.ImportanceSamplingCoreset, bc.UniformSamplingCoreset]
+anms = ['Full']
+algs = [bc.FullDataCoreset]
 algs_nms = zip(anms, algs)
 tests = [(N, algn) for N in [0, 1, 10] for algn in algs_nms]
 
 
-def coreset_single(N, algn):
-  anm, alg = algn
-  coreset = alg(N)
-  for m in [1, 10, 100]:
-    fd.build(m)
-    assert fd.error() < tol, "full wts failed: error not 0"
-    assert np.all(fd.weights() == np.ones(x.shape[0])), "full wts failed: weights not ones"
-  #check reset
-  fd.reset()
-  assert fd.M == 0 and np.all(np.fabs(fd.weights()) == 0.) and np.fabs(fd.error()) < tol and not fd.reached_numeric_limit, "FullDataset failed: reset() did not properly reset"
-
-def test_coreset():
-  for N, alg in tests:
-    for n in range(n_trials):
-      yield coreset_single, N, alg
-
-
+def test_full_data(N, algn):
+  for N in [0, 1, 10]:
+    coreset = bc.FullDataCoreset(N)
+    for m in [1, 10, 100]:
+      fd.build(m)
+      assert coreset.error() < tol, "full wts failed: error not 0"
+      assert np.all(coreset.weights() == np.ones(x.shape[0])), "full wts failed: weights not ones"
+    #check reset
+    coreset.reset()
+    assert coreset.M == N and np.all(np.fabs(fd.weights() - 1.) == 0.) and np.fabs(fd.error()) < tol and not fd.reached_numeric_limit, "FullDataset failed: reset() did not properly reset"
