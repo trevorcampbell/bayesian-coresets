076c6168e88e9fc9670a341cb7b3f82fdcc8b76c
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-04-07 16:54:58 -0700

    fixed imports in base

--- bayesiancoresets/base/__init__.py
+++ bayesiancoresets/base/__init__.py
@@ -1,4 +1,4 @@
 from .coreset import Coreset
 from .iterative import IterativeCoreset, SingleGreedyCoreset
 from .fulldataset import FullDataCoreset
-from .sampling import ImportanceSamplingCoreset, UniformSamplingCoreset
+from .sampling import SamplingCoreset
