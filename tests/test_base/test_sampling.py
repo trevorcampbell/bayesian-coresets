e32f0797aa9b5f6347ac85c17d3969452eac2c70
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-04-09 13:44:59 -0700

    minor ed

--- tests/test_base/test_sampling.py
+++ tests/test_base/test_sampling.py
@@ -16,9 +16,6 @@ class DummySamplingCoreset(SamplingCoreset):
   def _compute_sampling_probabilities(self):
     return np.ones(self.N)/self.N
   
-  def _update_cache(self):
-    pass
-
 def test_initialization():
   for N in [0, 1, 10]:
     coreset = DummySamplingCoreset(N)
@@ -51,8 +48,7 @@ def test_build():
     coreset = DummySamplingCoreset(N)
     for m in [1, 10, 100]:
       coreset.build(m)
-      assert (coreset.M == m and coreset.M == coreset.cts.sum()) or N == 0, "SamplingCoreset failed: M should always be number of samples taken: cts = " + str(coreset.cts.sum()) + " M = " + str(coreset.M)
       assert np.all(coreset.wts >= 0), "SamplingCoreset failed: weights must be nonnegative"
-      assert (coreset.wts > 0).sum() <= m, "SamplingCoreset failed: number of nonzero weights must be <= M: number = " + str((coreset.wts > 0).sum()) + " M = " + str(coreset.M)
+      assert (coreset.wts > 0).sum() <= m and (coreset.wts>0).sum() <= coreset.M, "SamplingCoreset failed: number of nonzero weights must be <= M: number = " + str((coreset.wts > 0).sum()) + " M = " + str(coreset.M)
 
 
