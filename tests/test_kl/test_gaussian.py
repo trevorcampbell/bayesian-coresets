f9764f1c99ce065d71c62513bf40f85134a0ad47
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-04-10 12:02:36 -0700

    edited wrong file; prev commit fix

--- tests/test_kl/test_gaussian.py
+++ tests/test_kl/test_gaussian.py
@@ -52,14 +52,14 @@ def ll_m2_exact_diag(muw, Sigw, Siginv, x):
   
 
 class ExactGaussianL1KLCoreset(bc.L1KLCoreset):
-  def __init__(self, x, mu0, Sig0, Sig):
+  def __init__(self, x, mu0, Sig0, Sig, reverse=True):
     self.x = x
     self.mu0 = mu0
     self.Sig0 = Sig0
     self.Sig0inv = np.linalg.inv(Sig0)
     self.Sig = Sig
     self.Siginv = np.linalg.inv(Sig)
-    super().__init__(None, None, 0)
+    super().__init__(potentials=None, sampler=None, n_samples=0, reverse=reverse)
 
   def _compute_scales(self):
     return np.sqrt(ll_m2_exact_diag(self.mu0, self.Sig0, self.Siginv, self.x))
@@ -81,16 +81,24 @@ class ExactGaussianL1KLCoreset(bc.L1KLCoreset):
       return g(w)/np.sqrt(ll_m2_exact_diag(muw, Sigw, self.Siginv, self.x))
     else:
       return g(w)
+
+class EGL1Reverse(ExactGaussianL1KLCoreset):
+  def __init__(self, x, mu0, Sig0, Sig): 
+    super().__init__(self, x, mu0, Sig0, Sig, reverse=True) 
+
+class EGL1Forward(ExactGaussianL1KLCoreset):
+  def __init__(self, x, mu0, Sig0, Sig):
+    super().__init__(self, x, mu0, Sig0, Sig, reverse=False) 
 
 class ExactGaussianGreedyKLCoreset(bc.GreedyKLCoreset):
-  def __init__(self, x, mu0, Sig0, Sig):
+  def __init__(self, x, mu0, Sig0, Sig, reverse=True):
     self.x = x
     self.mu0 = mu0
     self.Sig0 = Sig0
     self.Sig0inv = np.linalg.inv(Sig0)
     self.Sig = Sig
     self.Siginv = np.linalg.inv(Sig)
-    super().__init__(None, None, 0)
+    super().__init__(potentials=None, sampler=None, n_samples=0, reverse=reverse)
 
   def _compute_scales(self):
     return np.sqrt(ll_m2_exact_diag(self.mu0, self.Sig0, self.Siginv, self.x))
@@ -112,6 +120,15 @@ class ExactGaussianGreedyKLCoreset(bc.GreedyKLCoreset):
       return g(w)/np.sqrt(ll_m2_exact_diag(muw, Sigw, self.Siginv, self.x))
     else:
       return g(w)
+
+class EGGreedyReverse(ExactGaussianGreedyKLCoreset):
+  def __init__(self, x, mu0, Sig0, Sig): 
+    super().__init__(self, x, mu0, Sig0, Sig, reverse=True) 
+
+class EGGreedyForward(ExactGaussianGreedyKLCoreset):
+  def __init__(self, x, mu0, Sig0, Sig): 
+    super().__init__(self, x, mu0, Sig0, Sig, reverse=False) 
+
 
 
 warnings.filterwarnings('ignore', category=UserWarning) #tests will generate warnings (due to pathological data design for testing), just ignore them
@@ -122,15 +139,15 @@ tol = 1e-6
 
 
 n_trials = 10
-anms = ['GreedyKL', 'L1KL']
-algs = [ExactGaussianGreedyKLCoreset, ExactGaussianL1KLCoreset]
+anms = ['GreedyKLReverse', 'GreedyKLForward', 'L1KLReverse', 'L1KLForward']
+algs = [EGGreedyReverse, EGGreedyForward, EGL1Reverse, EGL1Forward]
 algs_nms = list(zip(anms, algs))
 tests = [(N, D, dist, algn) for N in [1, 10, 100] for D in [1, 3, 10] for dist in ['gauss', 'bin', 'gauss_colinear', 'bin_colinear', 'axis_aligned'] for algn in algs_nms]
 
 
 def gendata(N, D, dist="gauss"):
   if dist == "gauss":
-    x = np.random.normal(0., 1., (N, D))
+    x = np.random.normal(0., 1., (N, D)) 
   elif dist == "bin":
     x = (np.random.rand(N, D) > 0.5).astype(float)
   elif dist == "gauss_colinear":
@@ -145,7 +162,14 @@ def gendata(N, D, dist="gauss"):
     x = np.zeros((N, N))
     for i in range(N):
       x[i, i] = 1./float(N)
-  return x
+
+  mu0 = np.random.normal(0., 1.)
+  Sig0 = np.random.normal(0., 1., (D, D))
+  Sig0 = Sig0.T.dot(Sig0)
+  Sig = np.random.normal(0., 1., (D, D))
+  Sig = Sig.T.dot(Sig)
+
+  return x, mu0, Sig0, Sig
 
 ####################################################
 #verifies that 
@@ -158,70 +182,56 @@ def gendata(N, D, dist="gauss"):
 #-if input is colinear, error is 0 forall M
 ####################################################
 def coreset_single(N, D, dist, algn):
-  x = gendata(N, D, dist)
-  xs = x.sum(axis=0)
+  x, mu0, Sig0, Sig = gendata(N, D, dist)
+  Sig0inv = np.linalg.inv(Sig0)
+  Siginv = np.linalg.inv(Sig)
+  mup, Sigp = weighted_post(mu0, np.linalg.inv(Sig0), np.linalg.inv(Sig), x, np.ones(x.shape[0]))
   anm, alg = algn
-  coreset = alg(x, use_cached_xw=True)
-  accuratecoreset = alg(x, use_cached_xw=False)
+  coreset = alg(x, mu0, Sig0, Sig)
 
   #incremental M tests
   prev_err = np.inf
   for m in range(1, N+1):
     coreset.build(m)
-    accuratecoreset.build(m)
     #check if coreset for 1 datapoint is immediately optimal
     if x.shape[0] == 1:
-      assert np.fabs(coreset.weights(optimal_scaling=True) - np.array([1])) < tol or (np.fabs(coreset.weights(optimal_scaling=True) - np.array([0])) < tol and (x**2).sum() == 0.), anm +" failed: coreset not immediately optimal with N = 1. weights: " + str(coreset.weights(optimal_scaling=True))
+      assert np.fabs(coreset.weights() - np.array([1])) < tol or (np.fabs(coreset.weights() - np.array([0])) < tol and (x**2).sum() == 0.), anm +" failed: coreset not immediately optimal with N = 1. weights: " + str(coreset.weights(optimal_scaling=True))
     #check if coreset is valid
     assert (coreset.weights() > 0.).sum() <= m, anm+" failed: coreset size > m"
     assert (coreset.weights() > 0.).sum() == coreset.size(), anm+" failed: sum of coreset.weights()>0  not equal to size(): sum = " + str((coreset.weights()>0).sum()) + " size(): " + str(coreset.size())
     assert np.all(coreset.weights() >= 0.), anm+" failed: coreset has negative weights"
     
-    xw = (coreset.weights()[:, np.newaxis]*x).sum(axis=0)
-    xwopt = (coreset.weights(optimal_scaling=True)[:, np.newaxis]*x).sum(axis=0)
+    muw, Sigw = weighted_post(mu0, Sig0inv, Siginv, x, coreset.weights())
+    w = coreset.weights()
  
     #check if actual output error is monotone
-    assert np.sqrt(((xw-xs)**2).sum()) - prev_err < tol, anm+" failed: error is not monotone decreasing, err = " + str(np.sqrt(((xw-xs)**2).sum())) + " prev_err = " +str(prev_err) 
+    err = weighted_post_KL(mu0, Sig0inv, Siginv, x, w, reverse=True if 'Reverse' in anm else False)
+    assert err - prev_err < tol, anm+" failed: error is not monotone decreasing, err = " + str(err) + " prev_err = " +str(prev_err) 
 
     #check if coreset is computing error properly
-    #without optimal scaling
-    assert np.fabs(coreset.error() - np.sqrt(((xw-xs)**2).sum())) < tol, anm+" failed: x(w) est is not close to true x(w): est err = " + str(coreset.error()) + ' true err = ' + str(np.sqrt(((xw-xs)**2).sum()))
-    #with optimal scaling
-    assert np.fabs(coreset.error(optimal_scaling=True) - np.sqrt(((xwopt-xs)**2).sum())) < tol, anm+" failed: x(w) est is not close to true x(w) with optimal scaling: est err = " + str(coreset.error(optimal_scaling=True)) + ' true err = ' + str(np.sqrt(((xwopt-xs)**2).sum()))
-
-    #check if fast / accurate error estimates are close
-    #without optimal scaling
-    assert np.fabs(coreset.error() - accuratecoreset.error()) < tol*1000, anm+" failed: error(accurate/fast) do not return similar results: fast err = " + str(coreset.error()) + ' acc err = ' + str(accuratecoreset.error())
-    #with optimal scaling
-    assert np.fabs(accuratecoreset.error(optimal_scaling=True) - coreset.error(optimal_scaling=True)) < tol*1000, anm+" failed: error(accurate/fast) with optimal scaling do not return similar results: fast err = " + str(coreset.error(optimal_scaling=True)) + ' acc err = ' + str(accuratecoreset.error(optimal_scaling=True))
-
-    #ensure optimally scaled error is lower than  regular
-    assert coreset.error(optimal_scaling=True) - coreset.error() < tol, anm+" failed: optimal scaled coreset produces higher acc error than regular one. Optimal err = " + str(coreset.error(optimal_scaling=True)) + ' regular err: ' + str(coreset.error())
+    assert np.fabs(coreset.error() - err) < tol, anm+" failed: error est is not close to true err: est err = " + str(coreset.error()) + ' true err = ' + str(err)
 
     #if data are colinear, check if the coreset is optimal immediately
     if 'colinear' in dist and m >= 1:
-      assert np.sqrt(((xwopt-xs)**2).sum()) < tol, anm + " failed: colinear data, m>= 1 not immediately optimal:  optimal scaled err " + str(np.sqrt(((xwopt-xs)**2).sum())) + " tol = " + str(tol) + " m = " + str(m) + ' xwopt = ' + str(xwopt) + ' xs = ' + str(xs)
-    ##if data are axis aligned, 
-    #if 'axis' in dist:
-    #  assert np.all( np.fabs(coreset.weights()[ coreset.weights() > 0. ] - 1. ) < tol ), anm+" failed: on axis-aligned data, weights are not 1"
-    #  assert np.fabs(np.sqrt(((xw-xs)**2).sum())/np.sqrt((xs**2).sum()) - np.sqrt(1. - float(m)/float(N))) < tol, anm+" failed: on axis-aligned data, error is not sqrt(1 - M/N)"
-    prev_err = np.sqrt(((xw-xs)**2).sum())
-    prev_wts = coreset.wts.copy()
+      assert err < tol, anm + " failed: colinear data, m>= 1 not immediately optimal:  optimal err " + str(err) + " tol = " + str(tol) + " m = " + str(m)  
+    
+    prev_err = err
   #save incremental M result
   w_inc = coreset.weights()
-  xw_inc = (coreset.weights()[:, np.newaxis]*x).sum(axis=0) 
 
   #check reset
   coreset.reset()
-  assert coreset.M == 0 and np.all(np.fabs(coreset.weights()) == 0.) and np.fabs(coreset.error() - np.sqrt((xs**2).sum())) < tol and not coreset.reached_numeric_limit, anm+" failed: reset() did not properly reset"
+  err = weighted_post_KL(mu0, Sig0inv, Siginv, x, np.zeros(x.shape[0]), reverse=True if 'Reverse' in anm else False)
+  assert coreset.M == 0 and np.all(np.fabs(coreset.weights()) == 0.) and np.fabs(coreset.error() - err) < tol and not coreset.reached_numeric_limit, anm+" failed: reset() did not properly reset"
 
   #check build up to N all at once vs incremental
   #do this test for all except bin, where symmetries can cause instabilities in the choice of vector / weights
   if dist != 'bin':
     coreset.build(N)
-    xw = (coreset.weights()[:, np.newaxis]*x).sum(axis=0) 
-    assert np.sqrt(((xw-xw_inc)**2).sum()) < tol, anm+" failed: incremental buid up to N doesn't produce same result as one run at N : \n xw = " + str(xw) + " error = " +str(np.sqrt(((xw-xs)**2).sum())) + " \n xw_inc = " + str(xw_inc) + " error = " +  str(np.sqrt(((xw_inc-xs)**2).sum())) + " \n xs = " +str(xs)
-
+    w = coreset.weights()
+    err = weighted_post_KL(mu0, Sig0inv, Siginv, x, w, reverse=True if 'Reverse' in anm else False)
+    err_inc = weighted_post_KL(mu0, Sig0inv, Siginv, x, w_inc, reverse=True if 'Reverse' in anm else False)
+    assert np.sqrt(((w - w_inc)**2).sum()) < tol, anm+" failed: incremental buid up to N doesn't produce same result as one run at N : \n error = " +str(err) + " error_inc = " +  str(err_inc)
 
 def test_coreset():
   for N, D, dist, alg in tests:
