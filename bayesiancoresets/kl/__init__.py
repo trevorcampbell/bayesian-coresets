a8748c2c4bc306846abc5b113d03f6d63c9700e6
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-04-17 08:07:34 +0900

    fixed init

--- bayesiancoresets/kl/__init__.py
+++ bayesiancoresets/kl/__init__.py
@@ -1,3 +1,4 @@
+from .correctivegreedy import CorrectiveGreedyKLCoreset
 from .greedy import GreedyKLCoreset
 from .l1 import L1KLCoreset
 from .sampling import SamplingKLCoreset, UniformSamplingKLCoreset
