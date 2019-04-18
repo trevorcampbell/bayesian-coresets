5cbe9627b30e44443fd0d43f5128552f2f7ae8dc
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-04-18 22:46:16 +0900

    added adam prms to inits for gaussian kl egs

--- examples/gaussian_kl/sampling.py
+++ examples/gaussian_kl/sampling.py
@@ -6,14 +6,14 @@ from gaussian import *
  
 
 class EGS(bc.SamplingKLCoreset):
-  def __init__(self, x, mu0, Sig0, Sig):
+  def __init__(self, x, mu0, Sig0, Sig, adam_a1, adam_a2, opt_itrs):
     self.x = x
     self.mu0 = mu0
     self.Sig0 = Sig0
     self.Sig0inv = np.linalg.inv(Sig0)
     self.Sig = Sig
     self.Siginv = np.linalg.inv(Sig)
-    super().__init__(N=x.shape[0], potentials=None, sampler=None, n_samples=None, reverse=True, auto_above_N=False)
+    super().__init__(N=x.shape[0], potentials=None, sampler=None, n_samples=None, reverse=True, auto_above_N=False, adam_a1=adam_a1, adam_a2=adam_a2, opt_itrs=opt_itrs)
 
   #def _compute_scales(self):
   #  return np.sqrt(ll_m2_exact_diag(self.mu0, self.Sig0, self.Siginv, self.x))
@@ -37,7 +37,7 @@ class EGS(bc.SamplingKLCoreset):
       return g(w)
 
 class SGS(bc.SamplingKLCoreset):
-  def __init__(self, x, mu0, Sig0, Sig, n_samples):
+  def __init__(self, x, mu0, Sig0, Sig, n_samples, adam_a1, adam_a2, opt_itrs):
     self.x = x
     self.mu0 = mu0
     self.Sig0 = Sig0
@@ -49,17 +49,17 @@ class SGS(bc.SamplingKLCoreset):
     self.xSiginvx = (self.xSiginv*self.x).sum(axis=1)
     super().__init__(N = self.x.shape[0], potentials=lambda s : gaussian_potentials(self.Siginv, self.xSiginvx, self.xSiginv, self.logdetSig, self.x, s), 
                                      sampler=lambda w, n : gaussian_sampler(self.mu0, self.Sig0inv, self.Siginv, self.x, w, n), 
-                                     n_samples=n_samples, reverse=True, auto_above_N=False)
+                                     n_samples=n_samples, reverse=True, auto_above_N=False, adam_a1=adam_a1, adam_a2=adam_a2, opt_itrs=opt_itrs)
 
 class EGUS(bc.UniformSamplingKLCoreset):
-  def __init__(self, x, mu0, Sig0, Sig):
+  def __init__(self, x, mu0, Sig0, Sig, adam_a1, adam_a2, opt_itrs):
     self.x = x
     self.mu0 = mu0
     self.Sig0 = Sig0
     self.Sig0inv = np.linalg.inv(Sig0)
     self.Sig = Sig
     self.Siginv = np.linalg.inv(Sig)
-    super().__init__(N=x.shape[0], potentials=None, sampler=None, n_samples=None, reverse=True, auto_above_N=False)
+    super().__init__(N=x.shape[0], potentials=None, sampler=None, n_samples=None, reverse=True, auto_above_N=False, adam_a1=adam_a1, adam_a2=adam_a2, opt_itrs=opt_itrs)
 
   #def _compute_scales(self):
   #  return np.ones(self.x.shape[0])
--- examples/gaussian_kl/stochastic.py
+++ examples/gaussian_kl/stochastic.py
@@ -5,7 +5,7 @@ import warnings
 from gaussian import *
 
 class StochasticGaussianL1KLCoreset(bc.L1KLCoreset):
-  def __init__(self, x, mu0, Sig0, Sig, n_samples, reverse=True, scaled=True):
+  def __init__(self, x, mu0, Sig0, Sig, n_samples, reverse=True, scaled=True, adam_a1=1., adam_a2=1., opt_itrs=1000):
     self.x = x
     self.mu0 = mu0
     self.Sig0 = Sig0
@@ -17,18 +17,18 @@ class StochasticGaussianL1KLCoreset(bc.L1KLCoreset):
     self.xSiginvx = (self.xSiginv*self.x).sum(axis=1)
     super().__init__(N = self.x.shape[0], potentials=lambda s : gaussian_potentials(self.Siginv, self.xSiginvx, self.xSiginv, self.logdetSig, self.x, s), 
                                      sampler=lambda w, n : gaussian_sampler(self.mu0, self.Sig0inv, self.Siginv, self.x, w, n), 
-                                     n_samples=n_samples, reverse=reverse, scaled=scaled, auto_above_N=False)
+                                     n_samples=n_samples, reverse=reverse, scaled=scaled, auto_above_N=False, adam_a1=adam_a1, adam_a2=adam_a2, opt_itrs=opt_itrs)
 
 class SGL1Reverse(StochasticGaussianL1KLCoreset):
-  def __init__(self, x, mu0, Sig0, Sig, n_samples, scaled=True): 
-    super().__init__(x, mu0, Sig0, Sig, n_samples, True, scaled=scaled) 
+  def __init__(self, x, mu0, Sig0, Sig, n_samples, adam_a1, adam_a2, opt_itrs, scaled=True): 
+    super().__init__(x, mu0, Sig0, Sig, n_samples, True, scaled=scaled, adam_a1=adam_a1, adam_a2=adam_a2, opt_itrs=opt_itrs) 
 
 class SGL1Forward(StochasticGaussianL1KLCoreset):
-  def __init__(self, x, mu0, Sig0, Sig, n_samples, scaled=True):
-    super().__init__(x, mu0, Sig0, Sig, n_samples, False, scaled=scaled) 
+  def __init__(self, x, mu0, Sig0, Sig, n_samples, adam_a1, adam_a2, opt_itrs, scaled=True): 
+    super().__init__(x, mu0, Sig0, Sig, n_samples, False, scaled=scaled, adam_a1=adam_a1, adam_a2=adam_a2, opt_itrs=opt_itrs) 
 
 class StochasticGaussianGreedyKLCoreset(bc.GreedyKLCoreset):
-  def __init__(self, x, mu0, Sig0, Sig, n_samples, reverse=True):
+  def __init__(self, x, mu0, Sig0, Sig, n_samples, reverse=True, adam_a1=1., adam_a2=1., opt_itrs=1000):
     self.x = x
     self.mu0 = mu0
     self.Sig0 = Sig0
@@ -40,15 +40,15 @@ class StochasticGaussianGreedyKLCoreset(bc.GreedyKLCoreset):
     self.xSiginvx = (self.xSiginv*self.x).sum(axis=1)
     super().__init__(N = x.shape[0], potentials=lambda s : gaussian_potentials(self.Siginv, self.xSiginvx, self.xSiginv, self.logdetSig, self.x, s), 
                                      sampler=lambda w, n : gaussian_sampler(self.mu0, self.Sig0inv, self.Siginv, self.x, w, n), 
-                                     n_samples=n_samples, reverse=reverse, auto_above_N=False)
+                                     n_samples=n_samples, reverse=reverse, auto_above_N=False, adam_a1=adam_a1, adam_a2=adam_a2, opt_itrs=opt_itrs)
 
 class SGGreedyReverse(StochasticGaussianGreedyKLCoreset):
-  def __init__(self, x, mu0, Sig0, Sig, n_samples): 
-    super().__init__(x, mu0, Sig0, Sig, n_samples, True) 
+  def __init__(self, x, mu0, Sig0, Sig, n_samples, adam_a1, adam_a2, opt_itrs): 
+    super().__init__(x, mu0, Sig0, Sig, n_samples, True, adam_a1, adam_a2, opt_itrs) 
 
 class SGGreedyForward(StochasticGaussianGreedyKLCoreset):
-  def __init__(self, x, mu0, Sig0, Sig, n_samples): 
-    super().__init__(x, mu0, Sig0, Sig, n_samples, False) 
+  def __init__(self, x, mu0, Sig0, Sig, n_samples, adam_a1, adam_a2, opt_itrs): 
+    super().__init__(x, mu0, Sig0, Sig, n_samples, False, adam_a1, adam_a2, opt_itrs) 
 
 
 
