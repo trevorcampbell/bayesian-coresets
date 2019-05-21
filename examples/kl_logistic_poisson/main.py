91d4788a66248cae2ab1ee93d1a4b386f847ad48
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-05-20 22:43:00 -0700

    added comments to kl lr pr eg

--- examples/kl_logistic_poisson/main.py
+++ examples/kl_logistic_poisson/main.py
@@ -4,6 +4,7 @@ import bayesiancoresets as bc
 from scipy.optimize import minimize
 import time
 
+#adam optimizer with lambda fcn learning rate -- pulled from autograd library
 def adam(grad, x, num_iters, learning_rate, 
         b1=0.9, b2=0.999, eps=10**-8,callback=None):
     """Adam as described in http://arxiv.org/pdf/1412.6980.pdf.
@@ -20,6 +21,7 @@ def adam(grad, x, num_iters, learning_rate,
         x = x - learning_rate(i)*mhat/(np.sqrt(vhat) + eps)
     return x
 
+#computes KL( N(mu0, Sig0) || N(mu1, Sig1) )
 def gaussian_KL(mu0, Sig0, mu1, Sig1):
   t1 = np.dot(Sig1inv, Sig0).trace()
   t2 = np.dot((mu1-mu0),np.linalg.solve(Sig1, mu1-mu0))
@@ -27,13 +29,14 @@ def gaussian_KL(mu0, Sig0, mu1, Sig1):
   return 0.5*(t1+t2+t3-mu0.shape[0])
 
 
-#(wts[m,:][:,np.newaxis]*Z).mean(axis=0)[:D]
+#computes the Laplace approximation N(mu, Sig) to the posterior with weights wts
 def get_laplace(wts, Z, mu0)
   res = minimize(lambda mu : -log_joint(Z, mu, wts[m, :]), mu0, jac=lambda mu : -grad_log_joint(Z, mu, wts[m,:]))
   mu = res.x
   Sig = -np.linalg.inv(hess_log_joint(Z, mu))
   return mu, Sig
 
+#performs selection of the next datapoint to add to the riemann coreset
 def riemann_select(Z, w, muw, Sigw, n_samples):
   #take samples for empirical correlation estimation 
   samps = np.random.multivariate_normal(muw, Sigw, n_samples)
@@ -49,10 +52,11 @@ def riemann_select(Z, w, muw, Sigw, n_samples):
   stds = lls.std(axis=1)
   #compute correlations (w/o normalizing residual, since it doesn't affect selection)
   corrs = (lls*residuals).mean(axis=1)/stds
-  #for data in the active set, just look at abs(corr)
+  #for data in the active set, we look at abs(corr); for nonactive, only positive correlations are good
   corrs[w>0] = np.fabs(corrs[w>0])
   return corrs.argmax()
   
+#computes the gradient w.r.t. alpha, beta in the single-weight-update greedy method
 def grad_line(ab, Z, w, one_n, n_samps, muw):
   alpha = ab[0]
   beta = ab[1]
@@ -73,13 +77,15 @@ def grad_line(ab, Z, w, one_n, n_samps, muw):
   dKLda = -beta*(lls[n,:]*(one_f-wab_f)).mean()
   return np.array([dKLda, dKLdb])
 
+#runs the single-weight-update optimization for greedy
 def riemann_optimize_line(Z, w, n, muw, n_samples, adam_num_iters, adam_learning_rate):
   one_n = np.zeros(Z.shape[0])
   one_n[n] = 1.
   grad = lambda x : grad_line(x, Z, w, one_n, n_samples, muw)
   ab = adam(grad, np.array([0., 1.]), adam_num_iters, adam_learning_rate)
-  return ab[1]*(w+ab[0]*one_n), mu
+  return ab[1]*(w+ab[0]*one_n)
 
+#computes the gradient w.r.t. w (active indices only) for fully corrective greedy
 def grad_full(w, Z, n_samps, active_idcs, muw):
   #get samples from pi_w
   mu, Sig =  get_laplace(w, Z, muw)
@@ -97,6 +103,7 @@ def grad_full(w, Z, n_samps, active_idcs, muw):
   dKLdw[active_idcs] = (lls[active_idcs, :]*residuals).mean(axis=1)
   return dKLdw
 
+#runs the full weight reoptimization
 def riemann_optimize_full(Z, w, n, muw, n_samples, adam_num_iters, adam_learning_rate):
   active_idcs = w>0
   active_idcs[n] = True
@@ -105,11 +112,16 @@ def riemann_optimize_full(Z, w, n, muw, n_samples, adam_num_iters, adam_learning
   return w
 
 
-fldr = sys.argv[1]
-dnm = sys.argv[2]
-alg = sys.argv[3]
-ID = sys.argv[4]
+fldr = sys.argv[1] #should be either lr or poiss
+dnm = sys.argv[2] #if above is lr, should be synth / phishing / ds1; if above is poiss, should be synth, biketrips, or airportdelays
+alg = sys.argv[3] #should be hilbert / hilbert_corr / riemann / riemann_corr / uniform 
+ID = sys.argv[4] #just a number to denote trial #, any nonnegative integer
 
+#e.g.  python3 main.py lr phishing riemann_corr 2 
+#  will run fully corrective riemann coresets on logistic regression with the phishing dataset, trial # 2
+
+
+#load the logistic or poisson regression model depending on selected folder
 if fldr == 'lr':
   from model_lr import *
   print('Loading dataset '+dnm)
@@ -123,19 +135,23 @@ else:
   print('Loading posterior samples for '+dnm)
   samples = np.load('poiss/'+dnm+'_samples.npz')
 
-#fit a gaussian to the posterior samples, used for pihat smoothing
+#fit a gaussian to the posterior samples 
+#used for pihat computation for Hilbert coresets with noise to simulate uncertainty in a good pihat
 mup = samples.mean(axis=0)
 Sigp = np.cov(samples, rowvar=False)
-#create the prior -- also used for pihat smoothing
+#create the prior -- also used for the above purpose
 mu0 = np.zeros(mup.shape[0])
 Sig0 = np.eye(mup.shape[0])
 
-Ms = [1, 2, 5, 10, 20, 50, 100]
-projection_dim = 500 #random projection dimension
-pihat_noise = 0.15
-n_samples = 20
-adam_num_iters = 10000
-adam_learning_rate = lambda itr : 1./np.sqrt(itr+1.)
+###############################
+## TUNING PARAMETERS ##
+Ms = [1, 2, 5, 10, 20, 50, 100] #coreset sizes at which we record output
+projection_dim = 500 #random projection dimension for Hilbert csts
+pihat_noise = 0.15 #noise level (relative) for corrupting pihat
+n_samples = 20 #number of samples for KL gradients in ADAM optimization for riemann csts
+adam_num_iters = 10000 #number of ADAM optimization iterations in riemann csts
+adam_learning_rate = lambda itr : 1./np.sqrt(itr+1.) #ADAM learning rate in riemann csts
+###############################
 
 #initialize memory for coreset weights, laplace approx, kls
 wts = np.zeros((len(Ms), Z.shape[0]))
@@ -144,12 +160,14 @@ print('Building coresets via ' + alg)
 t0 = time.clock()
 if alg == 'hilbert' or 'hilbert_corr':
   #get pihat via interpolation between prior/posterior + noise
+  #uniformly smooth between prior and posterior
   U = np.random.rand()
   muhat = U*mup + (1.-U)*mu0
   Sighat = U*Sigp + (1.-U)*Sig0
+  #now corrupt the smoothed pihat
   muhat += pihat_noise*np.sqrt((muhat**2).sum())*np.random.randn(muhat.shape[0])
   Sighat *= np.exp(2*pihat_noise*np.random.randn())
-  #take pihat samples
+  #take pihat samples for random projection
   proj_samps = np.random.multivariate_normal(muhat, Sighat, projection_dim)
   #compute random projection
   lls = np.zeros((Z.shape[0], projection_dim))
@@ -161,24 +179,28 @@ if alg == 'hilbert' or 'hilbert_corr':
   for m in range(len(Ms)):
     print(str(m+1)+'/'+str(len(Ms)))
     giga.build(Ms[m])
+    #if we want to fully reoptimize in each step, call giga.optimize()
     if alg == 'hilbert_corr':
       giga.optimize() 
-    #record time
+    #record time and weights
     cputs[m] = time.clock()-t0
     wts[m, :] = giga.weights()
 elif alg == 'riemann' or 'riemann_corr':
   #normal dist for approx piw sampling; will use laplace throughout
-  muw = np.zeros(mup.shape[0])
-  Sigw = np.eye(mup.shape[0])
   w = np.zeros(Z.shape[0])
   for m in range(len(Ms)):
     #build up to Ms[m] one point at a time
     for j in range(Ms[m]-Ms[m-1] if m>0 else Ms[m]):
+      #get laplace w-posterior approx for sampling
+      muw, Sigw = get_laplace(w, Z)
+      #select next datapoint
       n = riemann_select(Z, w, muw, Sigw)
+      #optimize the weights
       if alg == 'riemann_corr':
         w = riemann_optimize_full(Z, w, n, muw, n_samples, adam_num_iters, adam_learning_rate)
       else:
         w = riemann_optimize_line(Z, w, n, muw, n_samples, adam_num_iters, adam_learning_rate)
+    #record the weights and cput time
     wts[m, :] = w.copy()
     #record time
     cputs[m] = time.clock()-t0
@@ -192,7 +214,8 @@ elif alg == 'uniform':
     #record time
     cputs[m] = time.clock() - t0
 
-#get laplace
+#get laplace approximations for each weight setting, and KL divergence to full posterior laplace approx mup Sigp
+#used for a quick/dirty performance comparison without expensive posterior sample comparisons (e.g. energy distance)
 mus_laplace = np.zeros((len(Ms), D))
 Sigs_laplace = np.zeros((len(Ms), D, D))
 kls_laplace = np.zeros(len(Ms))
