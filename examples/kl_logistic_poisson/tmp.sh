24d46282f739d143c13cc5594ebcab796ee7b09e
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-05-21 19:02:03 -0700

    added shell to run batch

---
+++ examples/kl_logistic_poisson/tmp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#3fldr = sys.argv[1] #should be either lr or poiss
+#3dnm = sys.argv[2] #if above is lr, should be synth / phishing / ds1; if above is poiss, should be synth, biketrips, or airportdelays
+#3alg = sys.argv[3] #should be hilbert / hilbert_corr / riemann / riemann_corr / uniform 
+#3ID = sys.argv[4] #just a number to denote trial #, any nonnegative integer
+
+
+for ID in {1..20}
+do
+    #for alg in "uniform" "hilbert" "hilbert_corr" "riemann" "riemann_corr"
+    for alg in "uniform" "hilbert" "hilbert_corr"
+    do
+        for fldrdnm in "lr synth" "lr phishing" "lr ds1" "poiss synth" "poiss biketrips" "poiss airportdelays"
+	do
+		python3 main.py $fldrdnm $alg $ID
+	done
+    done
+done
