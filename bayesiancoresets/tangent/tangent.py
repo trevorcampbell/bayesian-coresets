a0225adb97d5ec7db32bf8de9b19b7651163a24c
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-05-29 14:23:42 -0700

    rough draft of tangent done

--- bayesiancoresets/tangent/tangent.py
+++ bayesiancoresets/tangent/tangent.py
@@ -3,10 +3,8 @@ import warnings
 from .. import TOL
 
 class TangentSpace(object):
-  #keep track of a set of idcs (the tangent space subsample) and an optional "large" set of indices for more accurate vector sum computations
-  def __init__(self, w, d):
+  def __init__(self, d):
     self.alg_name = self.__class__.__name__
-    self.refw = w
     self.d = d
       
   #return the tangent vector for datapoint k (or slice)
@@ -18,21 +16,19 @@ class TangentSpace(object):
     else:
       raise KeyError
 
-  def update(self, w=None, d=None):
-    #TODO check if d is none/ w is none
-    if w != self.refw:
-      self.d = d
-      self.refw = w
-      self._w_and_dim_changed()
-    elif d != self.d:
-      self.d = d
-      self._dim_changed()
-     
+  #update the tangent space dimension
+  def change_dimension(self, d):
+    self.d = d
+    self._dim_changed()
+
+  def refresh(self, dim=None):
+    dim = dim if dim is not None else self.d
+    self.change_dimension(0)
+    self.change_dimension(dim)
+    return
+
   #methods to be implemented
   def _getslice(self, k):
-    raise NotImplementedError
-
-  def _w_and_dim_changed(self):
     raise NotImplementedError
 
   def _dim_changed(self):
@@ -48,21 +44,17 @@ class TangentSpace(object):
     return self.sum_w(w) - self.sum()
 
 
-#store fixed vectors, init takes vectors, get/set just returns np slices
-#warnings if many are 0 vectors (< TOL)
-#update dimension as an unimplemented method
-class ProjectedTangentSpace(TangentSpace):
-  def __init__(self, vecs):
-    super().__init__()
+class TangentSpaceProjection(TangentSpace):
+  def _set_vecs(self, vecs):
     if len(vecs.shape) != 2:
-      raise ValueError(self.alg_name+'.__init__(): vecs must be a 2d array, otherwise the expected behaviour is ambiguous')
+      raise ValueError(self.alg_name+'._set_vecs(): vecs must be a 2d array, otherwise the expected behaviour is ambiguous')
     self.vecs = vecs
     self.vsum = vecs.sum(axis=0)
     if ( np.sqrt((self.vecs**2).sum(axis=1)) < TOL).sum() > self.vecs.shape[0]*0.25:
       warnings.warn(self.alg_name+'.__init__(): more than 25% of the vectors have norm less than TOL. # = ' + str(np.sqrt((self.vecs**2).sum(axis=1)) < TOL).sum())
 
   def _getslice(self, k):
-    return vecs[k]
+    return vecs[k, :]
 
   def sum(self):
     return self.vsum
@@ -70,69 +62,49 @@ class ProjectedTangentSpace(TangentSpace):
   def sum_w(self, w):
     return w.dot(vecs[:w.shape[0], :])
 
-  def update_dimension(self):
-    raise NotImplementedError
+class MonteCarloTangentSpaceProjection(TangentSpaceProjection):
+  def __init__(self, log_likelihood, sampler, d):
+    super().__init__(d)
+    self.log_likelihood = log_likelihood
+    self.sampler = sampler
+    self._set_vecs(self.log_likelihood(sampler.sample(1)))
+    self._dim_changed()
 
-#run random feature projection to start, call parent init, then get/set
-#update dim via projection code below
-class RandomProjectedTangentSpace(ProjectedTangentSpace):
-  def __init__(self, ):
-    pass
+  def _dim_changed(self):
+    if self.d < self.vecs.shape[1]:
+      self._set_vecs(self.vecs[:, :self.d])
+    elif self.d > self.vecs.shape[1]:
+      old_dim = self.vecs.shape[1]
+      v = np.zeros((self.vecs.shape[0], self.d))
+      v[:, :old_dim] = self.vecs
+      v *= np.sqrt(old_dim)
+      v[:, old_dim:] = self.log_likelihood(sampler.sample(self.d-old_dim))
+      v /= np.sqrt(self.d)
+      self._set_vecs(v)
+    return
 
 #rather than random sampling for projection, do something smarter...
-class OptimizedProjectedTangentSpace(ProjectedTangentSpace):
+class OptimizedTangentSpaceProjection(TangentSpaceProjection):
   def __init__(self):
     raise NotImplementedError
 
 #noisy estimates of vectors, new random proj each time (avoids fixed error from above proj)
 #update dim just sets a fixed member d that tells random proj how many components to sample
 class MonteCarloTangentSpace(TangentSpace):
-  def __init__(self):
-    pass
-
-
-import numpy as np
-
-class Projection(object):
-
-  def __init__(self, data, log_likelihood, projection_dim, sample_approx_posterior):
-    self.data = data
+  def __init__(self, log_likelihood, sampler, d):
+    super().__init__(d)
     self.log_likelihood = log_likelihood
-    _Projection.__init__(self, data.shape[0], projection_dim, sample_approx_posterior)
+    self.sampler = sampler
+    self._dim_changed()
 
-  def _sample_component(self):
-    return self.log_likelihood(self.data, self.sample_approx_posterior())
+  def _dim_changed(self):
+    pass #do nothing in the MC tangent space, since vecs are always created fresh
 
+  def _getslice(self, k):
+    return self.log_likelihood(self.sampler(self.d), idcs=k)
 
-  def __init__(self, N, projection_dim, sample_approx_posterior):
-    self.dim = sample_approx_posterior().shape[0]
-    self.x = np.zeros((N, 0))
-    self.sample_approx_posterior = sample_approx_posterior
-    self.update_dimension(projection_dim)
-    return
-
-  def update_dimension(self, projection_dim):
-    if projection_dim < self.x.shape[1]:
-      self.x = self.x[:, :projection_dim]
-
-    if projection_dim > self.x.shape[1]:
-      old_dim = self.x.shape[1]
-      w = np.zeros((self.x.shape[0], projection_dim))
-      w[:, :old_dim] = self.x
-      w *= np.sqrt(old_dim)
-      for j in range(projection_dim-old_dim):
-          w[:, j+old_dim] = self._sample_component()
-      w /= np.sqrt(projection_dim)
-      self.x = w
-    return
-
-  def reset(self, projection_dim=None):
-    if projection_dim is None:
-      projection_dim = self.x.shape[1]
-    self.update_dimension(0)
-    self.update_dimension(projection_dim)
-    return
-
-  def get(self):
-    return self.x.copy()
+  def sum(self):
+    return self.log_likelihood(self.sampler(self.d)).sum(axis=0)
   
+  def sum_w(self, w):
+    return w.dot(self.log_likelihood(self.sampler(self.d)))
