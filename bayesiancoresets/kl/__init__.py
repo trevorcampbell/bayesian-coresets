8b82261af7b35a7b7793d9c6cf6e0b0606a166cb
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-05-29 00:33:31 -0700

    moved kl over to riemann

--- bayesiancoresets/kl/__init__.py
+++
@@ -1,4 +0,0 @@
-from .correctivegreedy import CorrectiveGreedyKLCoreset
-from .greedy import GreedyKLCoreset
-from .l1 import L1KLCoreset
-from .sampling import SamplingKLCoreset, UniformSamplingKLCoreset
