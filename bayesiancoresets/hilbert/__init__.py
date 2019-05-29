a422bcce83c80760a716a54a0fcd90d3d8efafb7
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-05-29 00:32:46 -0700

    moved hilbert methods over to hilbert fldr

---
+++ bayesiancoresets/hilbert/__init__.py
@@ -0,0 +1,9 @@
+from .frankwolfe import FrankWolfeCoreset
+from .sampling import VectorSamplingCoreset, VectorUniformSamplingCoreset
+from .projection import ProjectionF, Projection2
+from .giga import GIGACoreset
+from .forwardstagewise import ForwardStagewiseCoreset
+from .pursuit import MatchingPursuitCoreset
+from .orthopursuit import OrthoPursuitCoreset
+from .lar import LARCoreset
+from .lasso import LassoCoreset
--- bayesiancoresets/vector/__init__.py
+++
@@ -1,9 +0,0 @@
-from .frankwolfe import FrankWolfeCoreset
-from .sampling import VectorSamplingCoreset, VectorUniformSamplingCoreset
-from .projection import ProjectionF, Projection2
-from .giga import GIGACoreset
-from .forwardstagewise import ForwardStagewiseCoreset
-from .pursuit import MatchingPursuitCoreset
-from .orthopursuit import OrthoPursuitCoreset
-from .lar import LARCoreset
-from .lasso import LassoCoreset
