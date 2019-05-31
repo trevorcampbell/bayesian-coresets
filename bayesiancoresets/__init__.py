1ca89ebcd5e1fec8b5c1daafae2d8a4acd0dc36e
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-05-31 16:55:25 -0700

    adding a monkey patch for warnings.warn to not show stack trace

--- bayesiancoresets/__init__.py
+++ bayesiancoresets/__init__.py
@@ -1,6 +1,5 @@
 
 TOL = 1e-16
-WARNSTACKLEVEL = 0
 
 from .tangent import FixedFiniteTangentSpace, MonteCarloFiniteTangentSpace
 from .base import FullDataCoreset
---
+++ bayesiancoresets/utils/__init__.py
@@ -0,0 +1,1 @@
+from .warn import warn
