b4928e44e2de3427b3dcb1f348e118ba82736b0f
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-06-04 13:20:11 -0700

    hilb csts lr / poiss experiment working

--- examples/logistic_poisson_regression/main.py
+++ examples/logistic_poisson_regression/main.py
@@ -6,15 +6,16 @@ from inference import nuts, rhat, hmc
 import time
 
 
-## FOR LOGISTIC REGRESSION
-from model_lr import *
-dnames = ['synth', 'ds1', 'phishing']
-fldr = 'lr'
+### FOR LOGISTIC REGRESSION
+#from model_lr import *
+#dnames = ['synth', 'ds1', 'phishing']
+#fldr = 'lr'
 
-## FOR POISSON REGRESSION
-#from model_poiss import *
-#dnames = ['synth', 'airportdelays', 'biketrips']
-#fldr = 'poiss'
+# FOR POISSON REGRESSION
+from model_poiss import *
+dnames = ['synth', 'airportdelays', 'biketrips']
+fldr = 'poiss'
+
 
 n_trials = 20
 mcmc_steps = 5000 #total number of MH steps
@@ -26,7 +27,8 @@ step_size_init = 0.001
 n_leap = 15
 target_a = 0.8
 anms = ['GIGA', 'FW', 'RND']
-sampler = hmc #nuts
+algs = [bc.GIGACoreset, bc.FrankWolfeCoreset, bc.UniformSamplingCoreset]
+mcmc_alg = hmc #nuts
 
 for dnm in dnames:
   print('Loading dataset '+dnm)
@@ -39,6 +41,8 @@ for dnm in dnames:
   cov = -np.linalg.inv(hess_log_joint(Z, mu))
   t_laplace = time.time() - t0
   var_scales=np.ones(cov.shape[0])
+  #generate a sampler based on the laplace approx 
+  sampler = lambda sz : np.atleast_2d(np.random.multivariate_normal(mu, cov, sz))
 
   cputs = np.zeros((len(anms), n_trials, Ms.shape[0]))
   csizes = np.zeros((len(anms), n_trials, Ms.shape[0]))
@@ -49,10 +53,10 @@ for dnm in dnames:
   for tr in range(n_trials):
     print('Trial ' + str(tr+1) +'/' + str(n_trials))
 
-    print('Computing random projection')
+    print('Building tangent space projection')
     t0 = time.time()
-    proj = bc.ProjectionF(Z, grad_log_likelihood, projection_dim, lambda : np.random.multivariate_normal(mu, cov)) 
-    vecs = proj.get()
+    loglik = lambda th : np.hstack( [log_likelihood(Z, th[i,:])[:,np.newaxis] for i in range(th.shape[0])])
+    mct = bc.MonteCarloFiniteTangentSpace(loglik, sampler, projection_dim)
     t_projection = time.time()-t0
 
     print('Running MCMC on the full dataset')
@@ -60,7 +64,7 @@ for dnm in dnames:
     glogpZ = lambda th : grad_log_joint(Z, th, np.ones(Z.shape[0]))
     mcmc_param_init = np.random.multivariate_normal(mu, cov)
     t0 = time.time()
-    full_samples = sampler(logp = logpZ, gradlogp = glogpZ, 
+    full_samples = mcmc_alg(logp = logpZ, gradlogp = glogpZ, 
                      x0 = mcmc_param_init, sample_steps=mcmc_steps, burn_steps=mcmc_burn, adapt_steps=mcmc_burn, 
                      n_leapfrogs = n_leap, scale=var_scales, progress_bar=pbar, step_size=step_size_init, target_accept=target_a) 
     cputs_full[tr] = time.time()-t0
@@ -71,13 +75,7 @@ for dnm in dnames:
       print(anm +':')
 
       t0 = time.time()
-      alg = None
-      if 'GIGA' in anm:
-        alg = bc.GIGACoreset(vecs)
-      elif anm == 'FW':
-        alg = bc.FrankWolfeCoreset(vecs)
-      else:
-        alg = bc.VectorUniformSamplingCoreset(vecs)
+      alg = algs[aidx](mct)
       t_setup = time.time() - t0
 
       t_alg = 0.
@@ -87,15 +85,14 @@ for dnm in dnames:
         t0 = time.time()
         alg.build(Ms[m])
         t_alg += time.time()-t0
-        wts = alg.weights()
-        idcs = wts > 0
+        wts, idcs = alg.weights()
       
-        logpZ = lambda th : log_joint(Z[idcs, :], th, wts[idcs])
-        glogpZ = lambda th : grad_log_joint(Z[idcs, :], th, wts[idcs])
+        logpZ = lambda th : log_joint(Z[idcs, :], th, wts)
+        glogpZ = lambda th : grad_log_joint(Z[idcs, :], th, wts)
         mcmc_param_init = np.random.multivariate_normal(mu, cov)
         print('M = ' + str(Ms[m]) + ': MCMC')
         t0 = time.time()
-        th_samples = sampler(logp=logpZ, gradlogp=glogpZ, 
+        th_samples = mcmc_alg(logp=logpZ, gradlogp=glogpZ, 
                      x0 = mcmc_param_init, sample_steps=mcmc_steps, burn_steps=mcmc_burn, adapt_steps=mcmc_burn, 
                      n_leapfrogs= n_leap, scale=var_scales, progress_bar=pbar, step_size=step_size_init, target_accept=target_a)
         t_alg_mcmc = time.time()-t0    
@@ -103,9 +100,9 @@ for dnm in dnames:
         print('M = ' + str(Ms[m]) + ': CPU times')
         cputs[aidx, tr, m] = t_laplace + t_projection + t_setup + t_alg + t_alg_mcmc
         print('M = ' + str(Ms[m]) + ': coreset sizes')
-        csizes[aidx, tr, m] = (wts>0).sum()
+        csizes[aidx, tr, m] = wts.shape[0]
         print('M = ' + str(Ms[m]) + ': F norms')
-        gcs = np.array([ grad_log_joint(Z[idcs, :], full_samples[i, :], wts[idcs]) for i in range(full_samples.shape[0]) ])
+        gcs = np.array([ grad_log_joint(Z[idcs, :], full_samples[i, :], wts) for i in range(full_samples.shape[0]) ])
         gfs = np.array([ grad_log_joint(Z, full_samples[i, :], np.ones(Z.shape[0])) for i in range(full_samples.shape[0]) ])
         Fs[aidx, tr, m] = (((gcs - gfs)**2).sum(axis=1)).mean()
   #print(rhat(chains))
