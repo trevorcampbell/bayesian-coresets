fb15f93aaaab4348367ed270c5375afbc511c7e3
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-06-13 11:10:31 -0700

    setup keywds update

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'bayesiancoresets',
-    version='0.8',
+    version='0.9',
     description="Coresets for approximate Bayesian inference",
     author='Trevor Campbell',
-    author_email='tdjc@mit.edu',
+    author_email='trevor@stat.ubc.ca',
     url='https://github.com/trevorcampbell/bayesian-coresets/',
     #packages=['bayesiancoresets'],
     packages=find_packages(),
     install_requires=['numpy', 'scipy', 'scikit-learn'],
-    keywords = ['Bayesian', 'inference', 'coreset', 'Hilbert', 'Frank-Wolfe', 'greedy', 'geodesic'],
+    keywords = ['Bayesian', 'inference', 'coreset', 'sparse', 'variational inference', 'Riemann',  'Hilbert', 'Frank-Wolfe', 'greedy', 'geodesic'],
     platforms='ALL',
 )
