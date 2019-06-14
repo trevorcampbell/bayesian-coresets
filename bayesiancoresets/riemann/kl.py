b4f6575f93e834075ce76e0ac4ff69de2fe3a4d2
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-06-13 17:42:53 -0700

    fixed minor bug in kl

--- bayesiancoresets/riemann/kl.py
+++ bayesiancoresets/riemann/kl.py
@@ -3,14 +3,14 @@ from ..util.errors import NumericalPrecisionError
 from ..util.opt import nn_opt
 
 class KLCoreset(object):
-  def optimize(self):
+  def _optimize(self):
     x0 = self.wts
-    def grd(w):
-      T = self.tsf(w, self.idcs)
-      g = T.kl_grad(grad_idcs=self.idcs)
-      return g
-    x = nn_opt(x0, grd, opt_itrs=1000, step_sched = self.step_sched)
-    self._update(self.idcs, x)
+      def grd(w):
+        T = self.tsf(w, self.idcs)
+        g = T.kl_grad(grad_idcs=self.idcs)
+        return g
+      x = nn_opt(x0, grd, opt_itrs=self.opt_itrs, step_sched = self.step_sched)
+      self._update(self.idcs, x)
 
   def error(self):
     return 0.
