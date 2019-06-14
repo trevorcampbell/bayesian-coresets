471c15f6f714c1b128ff5ca50c22f49e10b2cd52
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-06-13 19:57:02 -0700

    added old quadratic greedy code

--- bayesiancoresets/riemann/kl.py
+++ bayesiancoresets/riemann/kl.py
@@ -5,12 +5,12 @@ from ..util.opt import nn_opt
 class KLCoreset(object):
   def _optimize(self):
     x0 = self.wts
-      def grd(w):
-        T = self.tsf(w, self.idcs)
-        g = T.kl_grad(grad_idcs=self.idcs)
-        return g
-      x = nn_opt(x0, grd, opt_itrs=self.opt_itrs, step_sched = self.step_sched)
-      self._update(self.idcs, x)
+    def grd(w):
+      T = self.tsf(w, self.idcs)
+      g = T.kl_grad(grad_idcs=self.idcs)
+      return g
+    x = nn_opt(x0, grd, opt_itrs=self.opt_itrs, step_sched = self.step_sched)
+    self._update(self.idcs, x)
 
   def error(self):
     return 0.
--- bayesiancoresets/util/__init__.py
+++ bayesiancoresets/util/__init__.py
@@ -1,5 +1,5 @@
 from .opt import nn_opt
-from .log import set_verbosity, #, set_repeat
+from .log import set_verbosity #, set_repeat
 
 TOL = 1e-12
 def set_tolerance(tol):
