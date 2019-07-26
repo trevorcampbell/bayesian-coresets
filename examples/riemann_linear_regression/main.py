269c840adee5b933fc439eec1364f88e597a9bfd
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-07-26 10:04:10 -0700

    coded first draft of lin reg; added plotting for housing price data; added lin reg model

--- examples/riemann_linear_regression/main.py
+++ examples/riemann_linear_regression/main.py
@@ -4,42 +4,70 @@ import os, sys
 from scipy.stats import multivariate_normal
 #make it so we can import models/etc from parent folder
 sys.path.insert(1, os.path.join(sys.path[0], '../common'))
-import gaussian
+import model_linreg
 
 
+#load data and compute true posterior
+#each row of x is [lat, lon, price]
+print('Loading data')
+x = np.load('../data/prices2018.npy')
+x[:, 2] = np.log10(x[:, 2])
 
-np.random.seed(1)
+datastd = x[:,2].std()
+datamn = x[:,2].mean()
 
-M = 30
-N = 1000
-d = 30
-n_samples = 1000
-trials = np.arange(100)
+#bases of increasing size; the last one is effectively a constant
+basis_unique_scales = np.array([.2, .4, .8, 1.2, 1.6, 2., 100])
+basis_unique_counts = np.array([1000, 500, 100, 50, 10, 5, 1])
 
-mu0 = np.zeros(d)
-Sig0 = np.eye(d)
-Sig = np.eye(d)
+#the dimension of the scaling vector for the above bases
+d = basis_unique_counts.sum()
+
+#model params
+mu0 = datamn*np.ones(d)
+Sig0 = np.diag(datastd**2+datamn**2)
+Sig = datastd**2*np.eye(d)
 SigL = np.linalg.cholesky(Sig)
-th = np.ones(d)
 Sig0inv = np.linalg.inv(Sig0)
 Siginv = np.linalg.inv(Sig)
 SigLInv = np.linalg.inv(SigL)
-opt_itrs = 3000
-proj_dim = 100
+
+#experiment params
+np.random.seed(1)
+M = 30
+n_samples = 10
+trials = np.arange(1)
+opt_itrs = 30
+proj_dim = 10
 pihat_noise =0.75
 
+
+trnum = 0
 for t in trials:
-  #generate data and compute true posterior
-  x = np.random.multivariate_normal(th, Sig, N)
-  mup, Sigp = gaussian.weighted_post(mu0, Sig0inv, Siginv, x, np.ones(x.shape[0]))
+  trnum += 1
+  #generate basis functions by uniformly randomly picking locations in the dataset
+  print('Trial ' + str(trnum) + '/' + str(trials.shape[0]))
+  print('Creating bases')
+  basis_scales = np.array([])
+  basis_locs = np.zeros((0,2))
+  for i in range(basis_unique_scales.shape[0]):
+    basis_scales = np.hstack((basis_scales, basis_unique_scales[i]*np.ones(basis_unique_counts[i])))
+    idcs = np.random.choice(np.arange(x.shape[0]), replace=False, size=basis_unique_counts[i])
+    basis_locs = np.vstack((basis_locs, x[idcs, :2]))
+
+  print('Converting bases and observations into X/Y matrices')
+  #convert basis functions + observed data locations into a big X matrix
+  X = np.zeros((x.shape[0], basis_scales.shape[0]))
+  for i in range(basis_scales.shape[0]):
+    X[:, i] = np.exp( -((x[:, :2] - basis_locs[i, :])**2).sum(axis=1) / (2*basis_scales[i]**2) )
+  Y = x[:, 2]
+
+  #get true posterior
+  mup, Sigp = model_linreg.weighted_post(mu0, Sig0inv, datastd**2, X, Y, np.ones(X.shape[0]))
   Sigpinv = np.linalg.inv(Sigp)
 
-  #compute constants for log likelihood function
-  xSiginv = x.dot(Siginv)
-  xSiginvx = (xSiginv*x).sum(axis=1)
-  logdetSig = np.linalg.slogdet(Sig)[1]
-
-  log_likelihood = lambda samples : gaussian.gaussian_potentials(Siginv, xSiginvx, xSiginv, logdetSig, x, samples)
+  #create function to output log_likelihood given param samples
+  log_likelihood = lambda samples : model_linreg.potentials(datastd**2, X, Y, samples)
 
   #create tangent space for well-tuned Hilbert coreset alg
   T_true = bc.MonteCarloFiniteTangentSpace(log_likelihood, lambda sz : np.random.multivariate_normal(mup, 9*Sigp, sz), proj_dim)
@@ -57,7 +85,7 @@ for t in trials:
   def tangent_space_factory(wts, idcs):
     w = np.zeros(x.shape[0])
     w[idcs] = wts
-    muw, Sigw = gaussian.weighted_post(mu0, Sig0inv, Siginv, x, w)
+    muw, Sigw = model_linreg.weighted_post(mu0, Sig0inv, datastd**2, X, Y, w)
     nu = (x - muw).dot(SigLInv.T)
     Psi = np.dot(SigLInv, np.dot(Sigw, SigLInv.T))
 
@@ -103,21 +131,22 @@ for t in trials:
     rklw = np.zeros(M+1)
     fklw = np.zeros(M+1)
     for m in range(M+1):
-      muw[m, :], Sigw[m, :, :] = gaussian.weighted_post(mu0, Sig0inv, Siginv, x, w[m, :])
-      rklw[m] = gaussian.weighted_post_KL(mu0, Sig0inv, Siginv, x, w[m, :], reverse=True)
-      fklw[m] = gaussian.weighted_post_KL(mu0, Sig0inv, Siginv, x, w[m, :], reverse=False)
+      muw[m, :], Sigw[m, :, :] = model_linreg.weighted_post(mu0, Sig0inv, datastd**2, X, Y, w[m, :])
+      rklw[m] = gaussian.weighted_post_KL(th0, Sig0inv, sigsq, X, Y, w[m,:], reverse=True)
+      fklw[m] = gaussian.weighted_post_KL(th0, Sig0inv, sigsq, X, Y, w[m,:], reverse=False)
     muw_opt = np.zeros((M+1, mu0.shape[0]))
     Sigw_opt = np.zeros((M+1,mu0.shape[0], mu0.shape[0]))
     rklw_opt = np.zeros(M+1)
     fklw_opt = np.zeros(M+1)
     for m in range(M+1):
-      muw_opt[m, :], Sigw_opt[m, :, :] = gaussian.weighted_post(mu0, Sig0inv, Siginv, x, w_opt[m, :])
-      rklw_opt[m] = gaussian.weighted_post_KL(mu0, Sig0inv, Siginv, x, w_opt[m, :], reverse=True)
-      fklw_opt[m] = gaussian.weighted_post_KL(mu0, Sig0inv, Siginv, x, w_opt[m, :], reverse=False)
+      muw_opt[m, :], Sigw_opt[m, :, :] = model_linreg.weighted_post(mu0, Sig0inv, datastd**2, X, Y, w_opt[m, :])
+      rklw_opt[m] = gaussian.weighted_post_KL(th0, Sig0inv, sigsq, X, Y, w_opt[m,:], reverse=True)
+      fklw_opt[m] = gaussian.weighted_post_KL(th0, Sig0inv, sigsq, X, Y, w_opt[m,:], reverse=False)
+
 
     if not os.path.exists('results/'):
       os.mkdir('results')
-    np.savez('results/results_'+nm+'_' + str(t)+'.npz', x=x, mu0=mu0, Sig0=Sig0, Sig=Sig, mup=mup, Sigp=Sigp, w=w, w_opt=w_opt,
+    np.savez('results/results_linreg_'+nm+'_' + str(t)+'.npz', x=x, mu0=mu0, Sig0=Sig0, Sig=Sig, mup=mup, Sigp=Sigp, w=w, w_opt=w_opt,
                                    muw=muw, Sigw=Sigw, rklw=rklw, fklw=fklw,
                                    muw_opt=muw_opt, Sigw_opt=Sigw_opt, rklw_opt=rklw_opt, fklw_opt=fklw_opt)
   
