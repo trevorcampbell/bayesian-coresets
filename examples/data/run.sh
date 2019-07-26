b3ccb2075adc7618f28e5cf72907633baf76c5af
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-07-25 23:40:35 -0700

    added full range to run

--- examples/data/run.sh
+++ examples/data/run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-#for bound in {0..1017357..50000}
-
-for bound in {0..100..20}
+for bound in {0..500000..50000}
 do
-	python3 process_housing_prices.py $bound $((bound+20)) > logfile-$bound-$((bound+20)).log 2>&1 &
-	#python3 process_housing_prices.py $bound $((bound+50000))
+	python3 process_housing_prices.py $bound $((bound+50000)) > logfile-$bound-$((bound+50000)).log 2>&1 &
 done
