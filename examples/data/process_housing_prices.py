c03e8abd0823faca7f1b96127b7f665a57ba1fae
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-07-26 00:34:17 -0700

    wrote my own geocoding script for parsing price paid uk data (old one took days, new one < 1min)

--- examples/data/process_housing_prices.py
+++ examples/data/process_housing_prices.py
@@ -1,30 +1,80 @@
 import numpy as np
-import pgeocode
 import sys
+import pandas as pd
 
 
+
+
+#load geocoding data and preprocess into sorted format
+
+geodata_fields = ['country code', 'postal_code', 'place_name',
+               'state_name', 'state_code', 'county_name', 'county_code',
+               'community_name', 'community_code',
+               'latitude', 'longitude', 'accuracy']
+
+print('loading full GB postcodes database')
+geodata = pd.read_csv('GB_FULL.txt', sep='\t', header=0, names=geodata_fields, dtype={'postal_code': str})
+
+post_code_to_int = lambda x : int(''.join([str(ord(a.lower())-97) if ord(a.lower())-97 >= 0 else str(ord(a.lower())-48)  for a in x.replace(' ', '')]))
+
+print('extracting postcode, lat, lon')
+geodata = geodata[['postal_code', 'latitude', 'longitude']]
+print('converting post codes to integers')
+geodata['postal_code'] = geodata['postal_code'].apply(post_code_to_int)
+print('sorting by integer tags')
+geodata.sort_values(by='postal_code', inplace=True)
+print('converting to np array')
+geodata = np.array(geodata)
+
+print('loading price paid data')
 f = open('pp-2018.csv', 'r')
 lines = f.readlines()
 f.close()
 
-start_idx = int(sys.argv[1])
-end_idx = int(sys.argv[2])
-
-lines = lines[start_idx:end_idx]
-
-#[latitude, longitude, price]
-data = np.zeros((len(lines), 3))
-nomi = pgeocode.Nominatim('gb_full')
+#[integer post code tag, price]
+data = np.zeros((len(lines), 2))
+print('extracting post code and price')
 for i in range(data.shape[0]):
   print('processing entry ' + str(i+1)+'/'+str(data.shape[0]))
-  sys.stderr.flush()
-  sys.stdout.flush()
   tokens = [s.strip(' "') for s in lines[i].split(',')]
   price = int(tokens[1])
-  postcode = tokens[3]
-  pgeo = nomi.query_postal_code(postcode)
-  data[i, :] = np.array([pgeo.latitude, pgeo.longitude, price])
+  try:
+    postcode = post_code_to_int(tokens[3])
+  except:
+    postcode = -1
+  data[i, :] = np.array([postcode, price])
 
-np.save('prices2018-'+str(start_idx)+'-'+str(end_idx)+'.npy', data)
+print('found ' + str(data.shape[0]) + ' entries')
+
+print('removing bad entries')
+data = data[data[:, 0] >= 0, :]
+
+print(str(data.shape[0]) + ' entries remaining')
+
+#sort by integer post tag
+print('sorting by integer post tag')
+data = data[data[:,0].argsort(), :]
+
+#now iterate through geodata and data, incrementing post tags on each as needed
+data_lat_lon = np.zeros((data.shape[0], 3))
+geo_idx = 0
+print('converting post code to lat lon')
+for i in range(data.shape[0]):
+  print('processing entry ' + str(i+1)+'/'+str(data.shape[0]))
+  while(geodata[geo_idx,0] < data[i,0]):
+    geo_idx += 1
+  if geodata[geo_idx,0] != data[i, 0]:
+    #geodata doesn't have this post code, give up
+    data_lat_lon[i, 0] = np.nan
+  else:    
+    data_lat_lon[i, 0] = geodata[geo_idx, 1]
+    data_lat_lon[i, 1] = geodata[geo_idx, 2]
+    data_lat_lon[i, 2] = data[i, 1]
+
+print('filtering bad entries')
+data_lat_lon = data_lat_lon[np.logical_not(np.isnan(data_lat_lon[:, 0])), :]
+print(str(data_lat_lon.shape[0]) + ' entries remaining')
+
+np.save('prices2018.npy', data_lat_lon)
 
 print('done')
--- examples/data/run.sh
+++ examples/data/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for bound in {0..500000..50000}
+for bound in {500000..1017357..50000}
 do
 	python3 process_housing_prices.py $bound $((bound+50000)) > logfile-$bound-$((bound+50000)).log 2>&1 &
 done
