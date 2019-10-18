fcc5bedeae295baa7f4802f38b006d50913c8436
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-10-18 13:37:51 -0700

    minor ed to runsh in rie gauss

--- examples/riemann_gaussian/run.sh
+++ examples/riemann_gaussian/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+#Algs: "SVI1" "SVIF" "GIGAT" "GIGAN" "RAND" "IH"
+
 for ID in {1..10}
 do
-    for alg in "SVI1" "SVIF" "GIGAT" "GIGAN" "RAND" "IH"
+    for alg in "SVI1" "SVIF" "GIGAT" "GIGAN" "RAND" 
     do
 	python3 main.py $alg $ID
     done
