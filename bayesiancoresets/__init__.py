57c613e213ff950b0394edf0bbb0fdcb17f95998
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-11-18 14:17:52 -0800

    renamed greedy to sparsevi

--- bayesiancoresets/__init__.py
+++ bayesiancoresets/__init__.py
@@ -1,3 +1,1 @@
-from .coreset import HilbertCoreset
-
-
+from .coreset import HilbertCoreset, SparseVICoreset, UniformSamplingCoreset
--- bayesiancoresets/coreset/__init__.py
+++ bayesiancoresets/coreset/__init__.py
@@ -2,3 +2,5 @@
 #from .quadratic_greedy import QuadraticSparseVICoreset
 #from .sampling import ImportanceSamplingKLCoreset, UniformSamplingKLCoreset
 from .hilbert import HilbertCoreset
+from .sampling import UniformSamplingCoreset
+from .sparsevi import SparseVICoreset
