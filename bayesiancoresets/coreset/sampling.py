b1c4881347f4f2e45ff50d7c4f084beba2b407e0
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-11-18 14:16:56 -0800

    added uniform sampling coreset w/o hilbert

---
+++ bayesiancoresets/coreset/sampling.py
@@ -0,0 +1,27 @@
+import numpy as np
+from ..util.errors import NumericalPrecisionError
+from .coreset import Coreset
+
+class UniformSamplingCoreset(Coreset):
+  def __init__(self, N, **kw):
+    super().__init__(**kw)
+    self.N = N
+    self.cts = []
+    self.ct_idcs = [] 
+
+  def reset(self):
+    self.cts = []
+    self.ct_idcs = []
+    super().reset()
+
+  def _build(self, itrs, sz):
+    if self.snnls.size()+itrs > sz:
+      raise ValueError(self.alg_name + '._build(): # itrs + current size cannot exceed total desired size sz. # itr = ' + str(itrs) + ' cur sz: ' + str(self.snnls.size()) + ' desired sz: ' + str(sz))
+    for i in range(itrs):
+      f = np.random.randint(N)
+      if f in self.ct_idcs:
+        self.cts[self.ct_idcs.index(f)] += 1
+      else:
+        self.ct_idcs.append(f)
+        self.cts.append(1)
+    self._overwrite(self.N*np.array(self.cts)/np.array(self.cts).sum(), np.array(self.ct_idcs))
