71c6ad8cdcd21574fccc5a7f0754e81a41c77981
Dionysios Manousakas <dm754@login-e-14.data.cluster>
2020-02-19 15:05:10 +0000

    logreg scripts modified for dppsvi

---
+++ examples/riemann_logistic_poisson_regression/dprun.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#fldr = sys.argv[1] #should be either lr or poiss
+#dnm = sys.argv[2] #if above is lr, should be synth / phishing / ds1; if above is poiss, should be synth, biketrips, or airportdelays
+#alg = sys.argv[3] #should be hilbert / hilbert_corr / riemann / riemann_corr / uniform 
+#ID = sys.argv[4] #just a number to denote trial #, any nonnegative integer
+
+
+for ID in {1..1}
+do
+    for alg in  "DPBPSVI" #"BPSVI" "SVI" "GIGAO" "GIGAR" "RAND" "PRIOR"
+    do
+        for dnm in "ds1.100" #"mnist2class_test" "fma"
+	do
+		python3 largemain.py $dnm $alg $ID 
+	done
+    done
+done
