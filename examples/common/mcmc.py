0f097d8cda95e57e4a1f50074db8cd535a405675
Dionysios Manousakas <dm754@login-e-11.data.cluster>
2020-02-19 10:02:27 +0000

    commented out reading Xt

--- examples/common/mcmc.py
+++ examples/common/mcmc.py
@@ -9,12 +9,12 @@ def load_data(dnm):
   data = np.load(dnm)
   X = data['X']
   Y = data['y']
-  Xt = data['Xt']
+  #Xt = data['Xt']
   #standardize the covariates; last col is intercept, so no stdization there
   m = X[:, :-1].mean(axis=0)
   V = np.cov(X[:, :-1], rowvar=False)+1e-12*np.eye(X.shape[1]-1)
   X[:, :-1] = np.linalg.solve(np.linalg.cholesky(V), (X[:, :-1] - m).T).T
-  Xt[:, :-1] = np.linalg.solve(np.linalg.cholesky(V), (Xt[:, :-1] - m).T).T
+  #Xt[:, :-1] = np.linalg.solve(np.linalg.cholesky(V), (Xt[:, :-1] - m).T).T
   data.close()
   return X[:, :-1], Y
 
