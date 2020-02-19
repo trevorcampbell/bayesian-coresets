fixing bugs in plot coreset pts / kl in rie gaus

examples/riemann_gaussian/plot_coreset_pts.py
@@ -1,4 +1,5 @@
 import bokeh.layouts as bkl
+import pickle as pk
 import bokeh.plotting as bkp
 import numpy as np
 import sys,os
@@ -7,24 +8,26 @@ sys.path.insert(1, os.path.join(sys.path[0], '../common'))
 from plotting import *
 
 
-size_x_axis = False
-trial_num = 1
-
-nm = ('SVI', 'SparseVI')
-Ms = [0, 1, 2, 5, 8, 12, 20, 50, 100, 150, 200]
-
 np.random.seed(5)
+trial_num = 1
+nm = ('SVI', 'SparseVI')
+#Ms = [0, 1, 2, 5, 8, 12, 20, 50, 100, 150, 200]
+Ms = [0, 1, 2]
+
 #plot the KL figure
 
 #plot the sequence of coreset pts and comparison of nonopt + opt
-res = np.load('results/results_'+nm[0] + '_' + str(trial_num)+'.npz')
-x = res['x']
-wt = res['w']
-Sig = res['Sig']
-mup = res['mup']
-Sigp = res['Sigp']
-muwt = res['muw']
-Sigwt = res['Sigw']
+f = open('results/results_'+nm[0]+'_' +str(trial_num)+'.pk', 'rb')
+res = pk.load(f)#res = (x, mu0, Sig0, Sig, mup, Sigp, w, p, muw, Sigw, rklw, fklw)
+f.close()
+x = res[0]
+wt = res[6]
+pt = res[7]
+Sig = res[3]
+mup = res[4]
+Sigp = res[5]
+muwt = res[8]
+Sigwt = res[9]
 
 #if dim x > 2, project onto two random orthogonal axes
 if x.shape[1] > 2:
@@ -49,6 +52,7 @@ if x.shape[1] > 2:
   for i in range(Sigwt.shape[0]):
     Sigwttmp[i,:,:] = a.T.dot(Sigwt[i,:,:].dot(a))
   Sigwt = Sigwttmp
+  pt = [p.dot(a) for p in pt]
   ##shift everything to be back to true th
   #true_th = true_th[:2]
   #x += true_th
@@ -62,20 +66,13 @@ for m in Ms:
   fig = bkp.figure(x_range=x_range, y_range=y_range, plot_width=750, plot_height=750)
   preprocess_plot(fig, '24pt', False, False)
 
-  msz = np.where((wt > 0).sum(axis=1) <= m)[0][-1]
   fig.scatter(x[:, 0], x[:, 1], fill_color='black', size=10, alpha=0.09)
 
-  if size_x_axis:
-    fig.scatter(x[:, 0], x[:, 1], fill_color='black', size=10*(wt[msz, :]>0)+40*wt[msz,:]/wt[msz,:].max(), line_color=None)
-  else:
-    fig.scatter(x[:, 0], x[:, 1], fill_color='black', size=10*(wt[msz, :]>0)+40*wt[m,:]/wt[m,:].max(), line_color=None)
+  fig.scatter(pt[m][:, 0], pt[m][:, 1], fill_color='black', size=10+40*wt[m]/wt[m].max(), line_color=None)
 
   plot_gaussian(fig, mup, (4./9.)*Sigp, (4./9.)*Sig, 'black', 17, 9, 1, 1, 'solid', 'Exact')
 
-  if size_x_axis:
-    plot_gaussian(fig, muwt[msz,:], (4./9.)*Sigwt[msz,:], (4./9.)*Sig, pal[0], 17, 9, 1, 1, 'solid', nm[1]+', size ' + str( (wt[msz, :]>0).sum() ))
-  else:
-    plot_gaussian(fig, muwt[m,:], (4./9.)*Sigwt[m,:], (4./9.)*Sig, pal[0], 17, 9, 1, 1, 'solid', nm[1]+', ' + str(m) +' pts') 
+  plot_gaussian(fig, muwt[m,:], (4./9.)*Sigwt[m,:], (4./9.)*Sig, pal[0], 17, 9, 1, 1, 'solid', nm[1]+', ' + str(m) +' pts') 
 
   postprocess_plot(fig, '24pt', orientation='horizontal', glyph_width=80)
   fig.legend.background_fill_alpha=0.