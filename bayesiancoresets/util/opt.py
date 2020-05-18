6f5a5a994c3b29093b070a90778022aac68263ac
Dionysios Manousakas <dm754@login-e-15.data.cluster>
2020-05-18 16:07:02 +0100

    most uptodate optimization class

--- bayesiancoresets/util/opt.py
+++ bayesiancoresets/util/opt.py
@@ -14,24 +14,21 @@ def nn_opt(x0, grd, opt_itrs=1000, step_sched = lambda i : 1./(i+1), b1=0.9, b2=
     m2 = b2*m2 + (1.-b2)*g**2
     upd = step_sched(i)*m1/(1.-b1**(i+1))/(eps + np.sqrt(m2/(1.-b2**(i+1))))
     x -= upd
-
     #project onto x>=0
     x = np.maximum(x, 0.)
-
   if verbose:
     sys.stdout.write('\n')
     sys.stdout.flush()
-
   return x
 
-def partial_nn_opt(x0, grd, nn_idcs, opt_itrs=1000, step_sched = lambda i : 1./(i+1), b1=0.9, b2=0.99, eps=1e-8, verbose=False):
+def partial_nn_opt(x0, grd, nn_idcs, opt_itrs=1000, step_sched=lambda i : 1./(i+1), b1=0.9, b2=0.999, eps=1e-8, verbose=False):
   x = x0.copy()
   m1 = np.zeros(x.shape[0])
   m2 = np.zeros(x.shape[0])
   for i in range(opt_itrs):
     g = grd(x)
     if verbose:
-      active_idcs = np.intersect1d(nn_idcs, np.where(x == 0)[0])
+      active_idcs = np.intersect1d(nn_idcs, np.where(x==0)[0])
       inactive_idcs = np.setdiff1d(np.arange(x.shape[0]), active_idcs)
       sys.stdout.write('itr ' + str(i+1) +'/'+str(opt_itrs)+': ||inactive constraint grads|| = ' + str(np.sqrt((g[inactive_idcs]**2).sum())) + '                \r')
       sys.stdout.flush()
@@ -39,12 +36,9 @@ def partial_nn_opt(x0, grd, nn_idcs, opt_itrs=1000, step_sched = lambda i : 1./(
     m2 = b2*m2 + (1.-b2)*g**2
     upd = step_sched(i)*m1/(1.-b1**(i+1))/(eps + np.sqrt(m2/(1.-b2**(i+1))))
     x -= upd
-
     #project onto x>=0
     x[nn_idcs] = np.maximum(x[nn_idcs], 0.)
-
   if verbose:
     sys.stdout.write('\n')
     sys.stdout.flush()
-
   return x
