1ff16aafbf8f01e3ef9d9b8cc653d1befd62c2f3
Trevor Campbell <trevor.d.campbell@gmail.com>
2020-05-19 12:40:52 -0700

    moved projector out of coreset fldr

--- bayesiancoresets/coreset/__init__.py
+++ bayesiancoresets/coreset/__init__.py
@@ -1,6 +1,4 @@
 from .hilbert import HilbertCoreset
 from .sampling import UniformSamplingCoreset
 from .sparsevi import SparseVICoreset
-from .projector import BlackBoxProjector, Projector
 from .bpsvi import BatchPSVICoreset
-from .dpbpsvi import DiffPrivBatchPSVICoreset
