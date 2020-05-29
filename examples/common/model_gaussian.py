3b3bed6408e3b307c1a9a326204d421c846e50a8
HaydenMcT <50335060+HaydenMcT@users.noreply.github.com>
2020-05-28 17:17:43 -0700

    standardizing model function names (still need to adjust calls to these functions in experiments)

--- examples/common/model_gaussian.py
+++ examples/common/model_gaussian.py
@@ -1,10 +1,10 @@
 import numpy as np
 import scipy.linalg as sl
 
-def gaussian_potentials(Siginv, xSiginvx, xSiginv, logdetSig, x, samples):
+def potentials(Siginv, xSiginvx, xSiginv, logdetSig, x, samples):
   return -x.shape[1]/2*np.log(2*np.pi) - 1./2.*logdetSig - 1./2.*(xSiginvx[:, np.newaxis] - 2.*np.dot(xSiginv, samples.T) + (np.dot(samples, Siginv)*samples).sum(axis=1))
 
-def gaussian_loglikelihood(x, th, Siginv, logdetSig):
+def log_likelihood(x, th, Siginv, logdetSig):
   x = np.atleast_2d(x)
   th = np.atleast_2d(th)
   xSiginvx = (x*(x.dot(Siginv))).sum(axis=1)
@@ -12,12 +12,12 @@ def gaussian_loglikelihood(x, th, Siginv, logdetSig):
   xSiginvth = x.dot(Siginv.dot(th.T))
   return -x.shape[1]/2*np.log(2*np.pi) - 1./2.*logdetSig - 1./2.*(xSiginvx[:, np.newaxis] + thSiginvth - 2*xSiginvth)
 
-def gaussian_grad_x_loglikelihood(x, th, Siginv):
+def grad_x_log_likelihood(x, th, Siginv):
   x = np.atleast_2d(x)
   th = np.atleast_2d(th)
   return th.dot(Siginv)[np.newaxis, :, :] - x.dot(Siginv)[:, np.newaxis, :]
  
-def gaussian_KL(mu0, Sig0, mu1, Sig1inv):
+def KL(mu0, Sig0, mu1, Sig1inv):
   t1 = np.dot(Sig1inv, Sig0).trace()
   t2 = np.dot((mu1-mu0),np.dot(Sig1inv, mu1-mu0))
   t3 = -np.linalg.slogdet(Sig1inv)[1] - np.linalg.slogdet(Sig0)[1]
--- examples/common/model_linreg.py
+++ examples/common/model_linreg.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy.linalg as sl
 
-def gaussian_loglikelihood(z, th, sigsq):
+def log_likelihood(z, th, sigsq):
   z = np.atleast_2d(z)
   x = z[:, :-1]
   y = z[:, -1]
@@ -9,14 +9,14 @@ def gaussian_loglikelihood(z, th, sigsq):
   XST = x.dot(th.T)
   return -1./2.*np.log(2.*np.pi*sigsq) - 1./(2.*sigsq)*(y[:,np.newaxis]**2 - 2*XST*y[:,np.newaxis] + XST**2)
 
-def gaussian_grad_x_loglikelihood(z, th, sigsq):
+def grad_x_log_likelihood(z, th, sigsq):
   z = np.atleast_2d(z)
   x = z[:, :-1]
   y = z[:, -1]
   th = np.atleast_2d(th)
   return 1./sigsq*(y[:, np.newaxis] - x.dot(th.T))[:,:,np.newaxis]*np.hstack((th, np.ones(th.shape[0])[:,np.newaxis]))[np.newaxis, :, :]
 
-def gaussian_KL(mu0, Sig0, mu1, Sig1inv):
+def KL(mu0, Sig0, mu1, Sig1inv):
   t1 = np.dot(Sig1inv, Sig0).trace()
   t2 = np.dot((mu1-mu0),np.dot(Sig1inv, mu1-mu0))
   t3 = -np.linalg.slogdet(Sig1inv)[1] - np.linalg.slogdet(Sig0)[1]
