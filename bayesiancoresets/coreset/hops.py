ce5d7d287217e82829135638e182e2d256dc2a5d
HaydenMcT <50335060+HaydenMcT@users.noreply.github.com>
2020-06-08 21:07:35 -0700

    A first draft at our new approach to HOPS

--- bayesiancoresets/coreset/hops.py
+++ bayesiancoresets/coreset/hops.py
@@ -4,73 +4,96 @@ from ..util.opt import nn_opt
 from .coreset import Coreset
 from scipy.optimize import nnls
 
+import numpy as np
+from ..util.errors import NumericalPrecisionError
+from ..util.opt import nn_opt
+from .coreset import Coreset
+
 class HOPSCoreset(Coreset):
-  def __init__(self, tangent_space_factory, opt_itrs, step_sched = lambda i : 1./(1.+i), update_single = False, **kw):
-    self.tsf = tangent_space_factory
+  def __init__(self, data, ll_projector, n_subsample_select=None, n_subsample_opt=None, opt_itrs=100, step_sched=lambda i : 1./(1.+i), **kw): 
+    self.data = data
+    self.ll_projector = ll_projector
+    self.n_subsample_select = None if n_subsample_select is None else min(data.shape[0], n_subsample_select)
+    self.n_subsample_opt = None if n_subsample_opt is None else min(data.shape[0], n_subsample_opt)
     self.step_sched = step_sched
     self.opt_itrs = opt_itrs
-    self.update_single = update_single
-    self.itrs_so_far = 0
     super().__init__(**kw)
 
-  def _build(self, itrs, sz):
-    if self.size()+itrs > sz: #change this to make ure we don't run more than M itertions, if our learning rate is i/M?
-      raise ValueError(self.alg_name + '._build(): # itrs + current size cannot exceed total desired size sz. # itr = ' + str(itrs) + ' cur sz: ' + str(self.size()) + ' desired sz: ' + str(sz))
+  def _build(self, itrs):
     for i in range(itrs):
       #search for the next best point
-      f = self._select()
-      #compute and update new weights
-      self._reweight(f) 
+      self._select()
+      #update the weights (using a faster heuristic rather than a full optimization)
+      self._hop(i) 
+    print("finished selecting points, optimizing coreset weights")
+    #at the very end, do a more rigorous optimization step
+    self.optimize()
+
+  def _get_projection(self, n_subsample, w, p):
+    #update the projector
+    self.ll_projector.update(w, p)
+
+    #construct a tangent space
+    if n_subsample is None:
+      sub_idcs = None
+      vecs = self.ll_projector.project(self.data)
+      sum_scaling = 1.
+    else:
+      sub_idcs = np.random.randint(self.data.shape[0], size=n_subsample)
+      vecs = self.ll_projector.project(self.data[sub_idcs])
+      sum_scaling = self.data.shape[0]/n_subsample
+
+    if self.pts.size > 0:
+      corevecs = self.ll_projector.project(self.pts)
+    else:
+      corevecs = np.zeros((0, vecs.shape[1]))
+
+    return vecs, sum_scaling, sub_idcs, corevecs
 
   def _select(self):
-    #construct a new tangent space for this search iteration
-    self.vecs = self.tsf(self.wts, self.idcs) # made vecs a field so that reqeight can access this same projection
-    #compute the correlations
-    resid = self.vecs.sum(axis=0) - self.wts.dot(self.vecs[self.idcs, :])
-    corrs = self.vecs.dot(resid) / np.sqrt((self.vecs**2).sum(axis=1)) / self.vecs.shape[1] #up to a constant; good enough for argmax
-    #for any in the active set, just look at magnitude
-    corrs[self.idcs] = np.fabs(corrs[self.idcs])
-    return np.argmax(corrs)
+    vecs, sum_scaling, sub_idcs, corevecs = self._get_projection(self.n_subsample_select, self.wts, self.pts)
 
-  # (assume reweight never called before select)
-  def _reweight(self, f): #had to add a  parameter to be able to scale appropriately
-    if f not in self.idcs: #(this was the part of the original code that had to stay, I hope I kepteveyrthing that was needed. Wasn't sure what reweight's job was originally intended to be
-      self._update(0., f)
-      
-    #print("shape of self.vecs: " + str(self.vecs.shape))
-    scaling_vector = self.step_sched(self.itrs_so_far)*np.ones(self.vecs.shape[0]) #not sure what self.n should be
-    #print("shape of scaling vector: " + str(scaling_vector.shape))
-    scaling_vector[self.idcs] += ((1 - self.step_sched(self.itrs_so_far)) * self.wts)
+    #compute the residual error
+    resid = sum_scaling*vecs.sum(axis=0) - self.wts.dot(corevecs)
 
-    #put the scaled vectors in their own new tangent space
-    scaled_tangent_space= scaling_vector[:, np.newaxis]*self.vecs #self.vecss is current ts?
-    
-    A = (scaled_tangent_space.T)[:,self.idcs]#np.matmul(self.vecs,self.vecs.T)[:,self.idcs]
-    b = scaled_tangent_space.sum(axis=0)
+    #compute the correlations for the new subsample
+    corrs = vecs.dot(resid) / np.sqrt((vecs**2).sum(axis=1)) / vecs.shape[1] #up to a constant; good enough for argmax
+    #compute the correlations for the coreset pts (use fabs because we can decrease the weight of these)
+    corecorrs = np.fabs(corevecs.dot(resid) / np.sqrt((corevecs**2).sum(axis=1))) / corevecs.shape[1] #up to a constant; good enough for argmax
 
+    #get the best selection; if it's an old coreset pt do nothing, if it's a new point expand and initialize storage for the new pt
+    if corecorrs.size == 0 or corrs.max() > corecorrs.max():
+      f = sub_idcs[np.argmax(corrs)] if sub_idcs is not None else np.argmax(corrs)
+      #expand and initialize storage for new coreset pt
+      #need to double-check that f isn't in self.idcs, since the subsample may contain some of the coreset pts
+      if f not in self.idcs:
+        self.wts.resize(self.wts.shape[0]+1)
+        self.idcs.resize(self.idcs.shape[0]+1)
+        self.pts.resize((self.pts.shape[0]+1, self.data.shape[1]))
+        self.wts[-1] = 0.
+        self.idcs[-1] = f
+        self.pts[-1] = self.data[f] 
+    return
 
-    result = nnls(A, b, maxiter=100*self.vecs.shape[1])
-    res = result[0]
-    # this was when we got back N-elected 0 weights mixed in, so to adjust we may have to do
-    weights = np.zeros(self.vecs.shape[0])
-    weights[self.idcs] = res
-    weights *= scaling_vector
-    self._overwrite(weights[self.idcs], self.idcs)
+  # Use some quick heuristic to assign weights to the coreset (such that the next selection step, which is based on the current coreset,
+  # picks a useful point)
+  # Coreset weights determine the distribution from which we sample when approximating the difference between the true posterior and our current coreset, 
+  # and also determine the difference between the true posterior and our current coreset (obviously). We want something reasonably accurate to the true posterior, as well as to the global optimum we could get with just these weights,
+  # but also we want to calculate this quickly and with stability (we don't want our coreset weights to be too sensitive to future changes)
+  def _hop(self, iter):
+    #currently, our idea is to target a tempered version of the true posterior, and solve this problem via sparse non-negative least squares
+    vecs, sum_scaling, sub_idcs, corevecs = self._get_projection(self.n_subsample_opt, self.wts, self.pts) #alternatively, we could use the same projection as we used in select
+    tempered_posterior = iter/self.data.shape[0] * sum_scaling *vecs.sum(axis=0)
+    self.wts = nnls(corevecs, tempered_posterior, maxiter = 100*self.vecs.shape[1])[0]
 
-    self.itrs_so_far += 1
-    
-  def _optimize(self): #this might be thrown off by the different step schedules we use for the rest of the problem - should there be two step schedules?
-    x0 = self.wts
+  def _optimize(self):
     def grd(w):
-      #construct the tangent space
-      vecs = self.tsf(w, self.idcs)
-      #compute residual
-      resid = vecs.sum(axis=0) - w.dot(vecs[self.idcs, :])
+      vecs, sum_scaling, sub_idcs, corevecs = self._get_projection(self.n_subsample_opt, w, self.pts)
+      resid = sum_scaling*vecs.sum(axis=0) - w.dot(corevecs)
       #output gradient of weights at idcs
-      return -vecs[self.idcs, :].dot(resid) / vecs.shape[1]
-    x = nn_opt(x0, grd, opt_itrs=self.opt_itrs, step_sched = self.step_sched)
-    self._update(x, self.idcs)
+      return -corevecs.dot(resid) / corevecs.shape[1]
+    x0 = self.wts
+    self.wts = nn_opt(x0, grd, opt_itrs = self.opt_itrs, step_sched = self.step_sched)
 
   def error(self):
     return 0. #TODO: implement KL estimate
-
