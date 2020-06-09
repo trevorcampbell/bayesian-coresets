cb57fbaa0be4579bb0f00bd3ee5e780b895c8b62
HaydenMcT <50335060+HaydenMcT@users.noreply.github.com>
2020-06-09 14:12:50 -0700

    renamed hpcgauss to main.py in riemann_gaussian, adjusted plotting script

--- examples/riemann_gaussian/plot_kl.py
+++ examples/riemann_gaussian/plot_kl.py
@@ -3,7 +3,7 @@ from bokeh.io import export_png, export_svgs
 import numpy as np
 import sys, os
 #make it so we can import models/etc from parent folder
-sys.path.insert(1, os.path.join(sys.path[0], '../bayesian-coresets-private/examples/common'))
+sys.path.insert(1, os.path.join(sys.path[0], '../common'))
 from plotting import *
 
 n_trials=int(sys.argv[1])
@@ -12,12 +12,10 @@ d=sys.argv[3]
 N=sys.argv[4]
 fldr=sys.argv[5]
 prfx=sys.argv[6]
-no_pcst=sys.argv[7]
 
 plot_reverse_kl = True
-trials = np.arange(1, n_trials)
-nms = [('BPSVI', 'PSVI'), ('SVI', 'SparseVI'),   ('RAND', 'Uniform'), ('GIGAO', 'GIGA (Optimal)'), ('GIGAR', 'GIGA (Realistic)')]
-pcsts = ['PSVI'] # pseudocoreset nameslist
+trials = np.arange(1, n_trials + 1)
+nms = [('RAND', 'Uniform'), ('GIGAO', 'GIGA(Optimal)'), ('GIGAR', "GIGA(Realistic)")]#('SVI', 'SparseVI'),   ('RAND', 'Uniform'), ('GIGAO', 'GIGA (Optimal)'), ('GIGAR', 'GIGA (Realistic)')]
 
 #plot the KL figure
 fig = bkp.figure(y_axis_type='log', plot_width=850, plot_height=850, x_axis_label='Coreset Size',
@@ -29,29 +27,23 @@ for i, nm in enumerate(nms):
   sz = []
   for tr in trials:
     numTuple = (prfx, nm[0], str(d), str(tr))
+    print(os.path.join(fldr, '_'.join(numTuple)+'.pk'))
     x_, mu0_, Sig0_, Sig_, mup_, Sigp_, w_, p_, muw_, Sigw_, rklw_, fklw_ = np.load(os.path.join(fldr, '_'.join(numTuple)+'.pk'), allow_pickle=True)
     if plot_reverse_kl:
       kl.append(rklw_[::plot_every])
     else:
       kl.append(fklw_[::plot_every])
-    if nm[0] in pcsts:
-      sz.append(range(len(w_))[::plot_every])
-    else:
-      sz.append([np.count_nonzero(a) for a in w_[::plot_every]])
+      
+    sz.append([np.count_nonzero(a) for a in w_[::plot_every]])
+
   x = np.percentile(sz, 50, axis=0)
-  # HACK FOR REPRODUCING COLOURS OF NEURIPS PAPER:
-  #assign PSVI to last colour of the pallete in order to maintain colouring of previous papers for baselines
-  if no_pcst=="True":
-    fig.line(x, np.percentile(kl, 50, axis=0), color=pal[i], line_width=5, legend=nm[1])
-    fig.patch(x = np.hstack((x, x[::-1])), y = np.hstack((np.percentile(kl, 75, axis=0), np.percentile(kl, 25, axis=0)[::-1])), color=pal[i], fill_alpha=0.4, legend=nm[1])
-  else:
-    fig.line(x, np.percentile(kl, 50, axis=0), color=pal[i-1], line_width=5, legend=nm[1])
-    fig.patch(x = np.hstack((x, x[::-1])), y = np.hstack((np.percentile(kl, 75, axis=0), np.percentile(kl, 25, axis=0)[::-1])), color=pal[i-1], fill_alpha=0.4, legend=nm[1])
+  fig.line(x, np.percentile(kl, 50, axis=0), color=pal[i-1], line_width=5, legend=nm[1])
+  fig.patch(x = np.hstack((x, x[::-1])), y = np.hstack((np.percentile(kl, 75, axis=0), np.percentile(kl, 25, axis=0)[::-1])), color=pal[i-1], fill_alpha=0.4, legend=nm[1])
 
 postprocess_plot(fig, '20pt', location='bottom_right', glyph_width=40)
 fig.legend.background_fill_alpha=0.
 fig.legend.border_line_alpha=0.
-fig.legend.visible = False
+fig.legend.visible = True
 
 bkp.show(fig)
 #fig.output_backend = "svg"
