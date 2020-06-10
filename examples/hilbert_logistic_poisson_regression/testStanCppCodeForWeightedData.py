42eae922ea927bd94c2bfbc7ac96300a419eb971
HaydenMcT <hayden.mctavish@alumni.ubc.ca>
2020-06-10 00:39:08 +0000

    simplification of logistic code, and adding more information to stanCppCode tests

--- examples/hilbert_logistic_poisson_regression/testStanCppCodeForWeightedData.py
+++ examples/hilbert_logistic_poisson_regression/testStanCppCodeForWeightedData.py
@@ -13,13 +13,15 @@ parser.add_argument('model', type=str, choices=["lr","poiss"], help="The regress
 parser.add_argument('dnm', type=str, help="The name of the dataset on which to run regression")
 parser.add_argument('ID', type=int, help="The trial number - used to initialize random number generation (for replicability)")
 parser.add_argument('numIdcs', type=int, help="number of indices to use in the weighted coreset")
-#TODO: make mcmc params command-line args
+parser.add_argument("mcmc_samples", type=int, help="number of MCMC samples to take using the sampler")
+#TODO: add a parameter for how many steps are warmup steps
 
 arguments = parser.parse_args()
 dnm = arguments.dnm
 ID = arguments.ID
 model = arguments.model
 numIdcs = arguments.numIdcs
+mcmc_samples = arguments.mcmc_samples
 
 #lrdnms = ['synth_lr', 'phishing', 'ds1', 'synth_lr_large', 'phishing_large', 'ds1_large']
 #prdnms = ['synth_poiss', 'biketrips', 'airportdelays', 'synth_poiss_large', 'biketrips_large', 'airportdelays_large']
@@ -30,27 +32,34 @@ elif model=="poiss":
 
 np.random.seed(ID)
 
-#should be command line arguments 
-mcmc_steps = 10000#10000 #total number of MH steps
-
 print('Loading dataset '+dnm)
 X,Y,Z, Zt, D = load_data('../data/'+dnm+'.npz')
+print("N, d: "+str(X.shape))
 
 idcs = np.random.choice(X.shape[0],numIdcs, replace=True) #random.choice is a bit slower without replacement, but may still be worth trying
 weights = np.random.randint(1,10,numIdcs)
 curX = X[idcs, :]
 curY = Y[idcs]
 #run the weighted version of sampler
-samples_using_code_for_weights = mcmc.sampler(dnm, curX, curY, mcmc_steps, stan_representation, weights=weights, seed = ID) 
+samples_using_code_for_weights = mcmc.sampler(dnm, curX, curY, mcmc_samples, stan_representation, weights=weights, seed = ID) 
 #build the modified dataset so that we can run the unweighted version of sampler
 for i in range(len(idcs)):
     toAdd = np.ones(weights[i]-1,dtype=int) * (i) #list i a number of times equal to the weight of the ith index
     curX = np.append(curX, curX[toAdd], axis=0)
     curY = np.append(curY, curY[toAdd])
-samples_using_standard_stan_code = mcmc.sampler(dnm, curX, curY, mcmc_steps, stan_representation, seed = ID)
+samples_using_standard_stan_code = mcmc.sampler(dnm, curX, curY, mcmc_samples, stan_representation, seed = ID)
 #samples_using_code_for_weights = mcmc.sampler(dnm, curX, curY, mcmc_steps, stan_representation, weights=np.ones(curX.shape[0], dtype=int), seed=ID)
-print("largest difference between samples using code for weights and samples using standard stan code (should be 0): ")
-print(np.max(samples_using_code_for_weights - samples_using_standard_stan_code))
-print("largest sample using standard stan code (if the previous value was not 0, our implementation may still be valid - just with minor numerical differences - if the value here is much bigger): ")
-print(np.max(samples_using_standard_stan_code))
+relative_err = np.abs(np.linalg.norm(samples_using_code_for_weights - samples_using_standard_stan_code, axis=1)/np.linalg.norm(samples_using_standard_stan_code,axis=1))
+print("largest relative error between samples using code for weights and samples using standard stan code (should be 0): ")
+print(np.max(relative_err))
+print("average relative error")
+print(np.mean(relative_err))
+print("median relative error")
+print(np.quantile(relative_err,.5))
+print("first quartile relative error")
+print(np.quantile(relative_err,.25))
+print("third quartile relative error")
+print(np.quantile(relative_err,.75))
+#print("largest sample using standard stan code (if the previous value was not 0, our implementation may still be valid - just with minor numerical differences - if the value here is much bigger): ")
+#print(np.max(samples_using_standard_stan_code))
 
