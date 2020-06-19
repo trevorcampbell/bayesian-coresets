893b7ff2a84214a7fdef468ad3fe8d38d93e3472
HaydenMcT <50335060+HaydenMcT@users.noreply.github.com>
2020-06-19 16:43:33 -0700

    Generalized plotting script beyond plotting KL divergence

--- examples/riemann_gaussian/plot.py
+++ examples/riemann_gaussian/plot.py
@@ -9,9 +9,14 @@ sys.path.insert(1, os.path.join(sys.path[0], '../common'))
 from plotting import *
 
 parser = argparse.ArgumentParser(description="Plots Riemannian linear regression experiments")
-parser.add_argument('names', type = str, nargs = '+', default = ["SVI", "RAND", "GIGAO", "GIGAR"], help = "a list of which algorithm names to plot results for (Examples: SVI / GIGAO / GIGAR / RAND)")
+parser.add_argument('--X', type = str, default="Iterations", help="The X axis of the plot - one of Iterations/Coreset Size/Forward KL/Reverse KL/CPU Time(s)")
+parser.add_argument('--use_log_scale_for_X', action='store_const', const=True, default = False, help = "If this flag is provided, The scale for X will be logarithmic instead of linear")
+parser.add_argument('--Y', type = str, default = "Reverse KL", help="The Y axis of the plot - one of Iterations/Coreset Size/Forward KL/Reverse KL/CPU Time(s)")
+parser.add_argument('--use_log_scale_for_Y', action='store_const', const=True, default = False, help = "If this flag is provided, The scale for Y will be logarithmic instead of linear")
 
+parser.add_argument('names', type = str, nargs = '+', default = ["SVI", "RAND", "GIGAO", "GIGAR"], help = "a list of which algorithm names to plot results for (Examples: SVI / GIGAO / GIGAR / RAND)")
 trials = parser.add_mutually_exclusive_group(required=True)
+
 trials.add_argument('--n_trials', type=int, help="Look for & plot experiments with trial IDs 1 through n_trials (inclusive)")
 trials.add_argument('--seeds', type = int, nargs = '+', help="Plot experiments associated with the provided trial numbers (seeds)")
 
@@ -22,14 +27,17 @@ parser.add_argument('--M', type=int, default='200', help='The desired maximum co
 parser.add_argument('--N', type=int, default='1000', help='Dataset size/number of examples for these experiments')
 parser.add_argument('--proj_dim', type=int, default = '100', help = "The number of samples taken when discretizing log likelihoods for these experiments")
 parser.add_argument('--SVI_opt_itrs', type=int, default = '500', help = '(If using SVI/HOPS) The number of iterations used when optimizing weights.')
-parser.add_argument('--fwd', action='store_const', const=True, default = False, help = 'If this flag is provided, will plot forward KL divergence instead of reverse KL divergence')
 parser.add_argument('--optimizing', action='store_const', const = True, default= False, help = 'If this flag is provided, plots results for algorithms that have been optimized by the HOPS optimize() function after the coreset was constructed')
 parser.add_argument('--SVI_step_sched', type=str, default = "lambda i : 1./(1+i)", help="Plots code with the associated step schedule (tuning rate) for SVI & HOPS. Default is \"lambda i : 1./(1+i)\", with the quotation marks.")
 parser.add_argument('--pihat_noise', type=float, default=.75, help = "(If plotting GIGAR or simulating another realistically tuned Hilbert Coreset) - plots data corresponding to this much noise being introduced to the smoothed pi-hat to make the sampler")
 
 arguments = parser.parse_args()
-trials = np.arange(1, arguments.n_trials + 1) if arguments.n_trials else arguments.seeds
+X = arguments.X
+use_log_scale_for_X = arguments.use_log_scale_for_X
+Y = arguments.Y
+use_log_scale_for_Y = arguments.use_log_scale_for_Y
 names = arguments.names
+trials = np.arange(1, arguments.n_trials + 1) if arguments.n_trials else arguments.seeds
 fldr = arguments.fldr
 plot_every = arguments.plot_every
 M = arguments.M
@@ -37,7 +45,6 @@ N = arguments.N
 d = arguments.d
 proj_dim = arguments.proj_dim
 SVI_opt_itrs =  arguments.SVI_opt_itrs
-fwd = arguments.fwd
 optimizing = arguments.optimizing
 pihat_noise = arguments.pihat_noise
 
@@ -55,8 +62,8 @@ for name in names:
   nms.append((name, algs[name]))
 
 #plot the KL figure
-fig = bkp.figure(y_axis_type='log', plot_width=850, plot_height=850, x_axis_label='Coreset Size',
-       y_axis_label=('Forward KL' if fwd else 'Reverse KL'), toolbar_location=None )
+fig = bkp.figure(y_axis_type='log' if use_log_scale_for_Y else 'linear', x_axis_type='log' if use_log_scale_for_X else 'linear',
+       plot_width=850, plot_height=850, x_axis_label=X, y_axis_label=Y, toolbar_location=None )
 preprocess_plot(fig, '32pt', False, True)
 
 for i, nm in enumerate(nms):
@@ -66,17 +73,15 @@ for i, nm in enumerate(nms):
     numTuple = (nm[0], "tr="+str(tr), "N="+str(N), "d="+str(d), "proj_dim="+str(proj_dim), "optimizing="+str(optimizing), "SVI_opt_itrs="+str(SVI_opt_itrs), 'SVI_step_sched_hash_sha1='+hashlib.sha1(arguments.SVI_step_sched.encode('utf-8')).hexdigest(), 'pihat_noise='+str(pihat_noise))
     print(os.path.join(fldr, '_'.join(numTuple)+'.pk'))
     x_, mu0_, Sig0_, Sig_, mup_, Sigp_, w_, p_, muw_, Sigw_, rklw_, fklw_, cputs_, tr_, N_, d_, proj_dim_, optimizing_, SVI_opt_itrs_, SVI_step_sched_, pihat_noise_ = np.load(os.path.join(fldr, '_'.join(numTuple)+'.pk'), allow_pickle=True)
-    if fwd:
-      kl.append(fklw_[::plot_every])
-    else:
-      kl.append(rklw_[::plot_every])
-      
-    sz.append(np.arange(1,len(rklw_)+1,plot_every))
-    #sz.append([np.count_nonzero(a) for a in w_[::plot_every]])
-
-  x = np.percentile(sz, 50, axis=0)
-  fig.line(x, np.percentile(kl, 50, axis=0), color=pal[i-1], line_width=5, legend=nm[1])
-  fig.patch(x = np.hstack((x, x[::-1])), y = np.hstack((np.percentile(kl, 75, axis=0), np.percentile(kl, 25, axis=0)[::-1])), color=pal[i-1], fill_alpha=0.4, legend=nm[1])
+    data = { 'Iterations': [np.arange(1,len(rklw_)+1,plot_every)],
+             'Coreset Size': [[np.count_nonzero(a) for a in w_[::plot_every]]],
+             'Forward KL': [fklw_[::plot_every]],
+             'Reverse KL': [rklw_[::plot_every]],
+             'CPU Time(s)': [cputs_[::plot_every]]}
+             
+  x = np.percentile(data[X], 50, axis=0)
+  fig.line(x, np.percentile(data[Y], 50, axis=0), color=pal[i-1], line_width=5, legend=nm[1])
+  fig.patch(x = np.hstack((x, x[::-1])), y = np.hstack((np.percentile(data[Y], 75, axis=0), np.percentile(data[Y], 25, axis=0)[::-1])), color=pal[i-1], fill_alpha=0.4, legend=nm[1])
 
 postprocess_plot(fig, '12pt', location='bottom_left', glyph_width=40)
 fig.legend.background_fill_alpha=0.
