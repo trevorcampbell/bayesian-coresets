Implementation of 0 to 1 scaling, and some minor fixes to plotting/experiment code

bayesiancoresets/coreset/hops.py
@@ -10,14 +10,15 @@ from ..util.opt import nn_opt
 from .coreset import Coreset
 
 class HOPSCoreset(Coreset):
-  def __init__(self, data, ll_projector, n_subsample_select=None, n_subsample_opt=None, opt_itrs=100, step_sched=lambda i : 1./(1.+i), **kw): 
+  def __init__(self, data, ll_projector, n_subsample_select=None, n_subsample_opt=None, opt_itrs=100, step_sched=lambda i : 1./(1.+i), scale_tempering_from_0_to_1 = False, **kw): 
     self.data = data
     self.ll_projector = ll_projector
     self.n_subsample_select = None if n_subsample_select is None else min(data.shape[0], n_subsample_select)
     self.n_subsample_opt = None if n_subsample_opt is None else min(data.shape[0], n_subsample_opt)
     self.step_sched = step_sched
     self.opt_itrs = opt_itrs
-    self.cur_itrs = 1
+    self.iteration_number = 1 #(if scale_tempering_from_0_to_1 is False) tracks how many iterations have been performed, to appropriately scale the tempering
+    self.scale_tempering_from_0_to_1 = scale_tempering_from_0_to_1
     super().__init__(**kw)
 
   def _build(self, itrs):
@@ -25,8 +26,15 @@ class HOPSCoreset(Coreset):
       #search for the next best point
       self._select()
       #update the weights (using a faster heuristic rather than a full optimization)
-      self._hop(self.cur_itrs)
-      self.cur_itrs += 1 
+      if self.scale_tempering_from_0_to_1:
+        #(assumes that _build is only called once, with "itrs" equal to the maximum allowable coreset size)
+        #we want to target linearly scaling temperings of our coreset (up to 1, skipping a scaling of 0)
+        tempering = np.linspace(0,1,itrs+1)[i+1]
+      else:
+        # scale the next hop by the current iteration count relative to the full dataset (but make sure we end at 1)
+        tempering = self.iteration_number/self.data.shape[0]
+        self.iteration_number += 1 
+      self._hop(tempering)
 
   def _get_projection(self, n_subsample, w, p):
     #update the projector
@@ -79,10 +87,10 @@ class HOPSCoreset(Coreset):
   # Coreset weights determine the distribution from which we sample when approximating the difference between the true posterior and our current coreset, 
   # and also determine the difference between the true posterior and our current coreset (obviously). We want something reasonably accurate to the true posterior, as well as to the global optimum we could get with just these weights,
   # but also we want to calculate this quickly and with stability (we don't want our coreset weights to be too sensitive to future changes)
-  def _hop(self, iter):
+  def _hop(self, tempering):
     #currently, our idea is to target a tempered version of the true posterior, and solve this problem via sparse non-negative least squares
     vecs, sum_scaling, sub_idcs, corevecs = self._get_projection(self.n_subsample_opt, self.wts, self.pts) #alternatively, we could use the same projection as we used in select
-    tempered_posterior = iter/self.data.shape[0] * sum_scaling *vecs.sum(axis=0)
+    tempered_posterior = tempering * sum_scaling *vecs.sum(axis=0)
     self.wts = nnls(corevecs.T, tempered_posterior.T, maxiter = 100*vecs.shape[1])[0]
 
   #at the very end, after having selected all our coreset points, we can do a more rigorous optimization step for the weights

examples/riemann_gaussian/plot_kl.py
@@ -8,8 +8,12 @@ sys.path.insert(1, os.path.join(sys.path[0], '../common'))
 from plotting import *
 
 parser = argparse.ArgumentParser(description="Plots Riemannian linear regression experiments")
-parser.add_argument('n_trials', type=int, help="This script will look for & plot experiments with trial IDs 1 through n_trials (inclusive)")
 parser.add_argument('names', type = str, nargs = '+', default = ["SVI", "RAND", "GIGAO", "GIGAR"], help = "a list of which algorithm names to plot results for (Examples: SVI / GIGAO / GIGAR / RAND)")
+
+trials = parser.add_mutually_exclusive_group(required=True)
+trials.add_argument('--n_trials', type=int, help="Look for & plot experiments with trial IDs 1 through n_trials (inclusive)")
+trials.add_argument('--seeds', type = int, nargs = '+', help="Plot experiments associated with the provided trial numbers (seeds)")
+
 parser.add_argument('--fldr', type=str, default="results/", help="This script will look for & plot experiments in this folder")
 parser.add_argument('--plot_every', type=int, default='1', help="Coarseness of the graph - will skip (plot_every-1) points between each plotted point")
 parser.add_argument('--d', type=int, default = '200', help="The dimension of the multivariate normal distribution used for these experiments")
@@ -19,9 +23,8 @@ parser.add_argument('--proj_dim', type=int, default = '100', help = "The number 
 parser.add_argument('--SVI_opt_itrs', type=int, default = '500', help = '(If using SVI/HOPS) The number of iterations used when optimizing weights.')
 parser.add_argument('--fwd', action='store_const', const=True, default = False, help = 'If this flag is provided, will plot forward KL divergence instead of reverse KL divergence')
 
-
 arguments = parser.parse_args()
-n_trials = arguments.n_trials
+trials = np.arange(1, arguments.n_trials + 1) if arguments.n_trials else arguments.seeds
 names = arguments.names
 fldr = arguments.fldr
 plot_every = arguments.plot_every
@@ -30,6 +33,7 @@ N = arguments.N
 d = arguments.d
 proj_dim = arguments.proj_dim
 SVI_opt_itrs =  arguments.SVI_opt_itrs
+fwd = arguments.fwd
 
 algs = {'SVIEXACT': 'Sparse VI (Exact Tangent Space)',
         'SVI': 'Sparse VI', 
@@ -37,17 +41,16 @@ algs = {'SVIEXACT': 'Sparse VI (Exact Tangent Space)',
         'GIGAR': "GIGA(Realistic)", 
         'RAND': "Uniform", 
         'HOPS': "HOPS",
-        'HOPSEXACT': "HOPS (Exact Tangent Space)"}
+        'HOPSEXACT': "HOPS (Exact Tangent Space)",
+        'HOPS_full_scaling': "HOPS (Tempering from 0-1)",
+        'HOPS_full_scaling_exact': "HOPS (Exact TS, Tempering from 0-1)"}
 nms = []
 for name in names:
   nms.append((name, algs[name]))
 
-plot_reverse_kl = not arguments.fwd
-trials = np.arange(1, n_trials + 1)
-
 #plot the KL figure
 fig = bkp.figure(y_axis_type='log', plot_width=850, plot_height=850, x_axis_label='Coreset Size',
-       y_axis_label=('Reverse KL' if plot_reverse_kl else 'Forward KL'), toolbar_location=None )
+       y_axis_label=('Forward KL' if fwd else 'Reverse KL'), toolbar_location=None )
 preprocess_plot(fig, '32pt', False, True)
 
 for i, nm in enumerate(nms):
@@ -57,25 +60,21 @@ for i, nm in enumerate(nms):
     numTuple = (nm[0], str(d), str(tr), str(N), str(proj_dim), str(SVI_opt_itrs))
     print(os.path.join(fldr, '_'.join(numTuple)+'.pk'))
     x_, mu0_, Sig0_, Sig_, mup_, Sigp_, w_, p_, muw_, Sigw_, rklw_, fklw_ = np.load(os.path.join(fldr, '_'.join(numTuple)+'.pk'), allow_pickle=True)
-    if plot_reverse_kl:
-      kl.append(rklw_[::plot_every])
-    else:
+    if fwd:
       kl.append(fklw_[::plot_every])
+    else:
+      kl.append(rklw_[::plot_every])
       
-    sz.append([np.count_nonzero(a) for a in w_[::plot_every]])
+    sz.append(np.arange(1,len(rklw_)+1,plot_every))
+    #sz.append([np.count_nonzero(a) for a in w_[::plot_every]])
 
   x = np.percentile(sz, 50, axis=0)
   fig.line(x, np.percentile(kl, 50, axis=0), color=pal[i-1], line_width=5, legend=nm[1])
   fig.patch(x = np.hstack((x, x[::-1])), y = np.hstack((np.percentile(kl, 75, axis=0), np.percentile(kl, 25, axis=0)[::-1])), color=pal[i-1], fill_alpha=0.4, legend=nm[1])
 
-postprocess_plot(fig, '20pt', location='bottom_right', glyph_width=40)
+postprocess_plot(fig, '12pt', location='bottom_right', glyph_width=40)
 fig.legend.background_fill_alpha=0.
 fig.legend.border_line_alpha=0.
 fig.legend.visible = True
 
 bkp.show(fig)
-#fig.output_backend = "svg"
-#fldr_figs = 'figs'
-#if not os.path.exists(fldr_figs):
-#  os.mkdir(fldr_figs)
-#export_png(fig, filename=os.path.join(fldr_figs, "d"+str(d)+"_KLDvsCstSize.png"), height=1500, width=1500)