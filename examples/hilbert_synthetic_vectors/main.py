c6125d8ec0eb780161628569d93b8c32bf2e25e0
HaydenMcT <50335060+HaydenMcT@users.noreply.github.com>
2020-06-25 19:36:47 -0700

    basic fixes to example (command line arguments, no loops within the example)

--- examples/hilbert_synthetic_vectors/main.py
+++ examples/hilbert_synthetic_vectors/main.py
@@ -2,6 +2,21 @@ from __future__ import print_function
 import numpy as np
 import bayesiancoresets as bc
 import time
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('tr', type=int, help='The trial number (used to seed random number generation)')
+parser.add_argument('alg_nm', type=str, choices=['FW', 'GIGA', 'OMP', 'IS', 'US'], help="The sparse non negative least squares algorithm to use: one of FW (Frank Wolfe), GIGA (Greedy Iterative Geodeic Ascent), OMP (Orthogonal Matching Pursuit), IS (Importance Sampling), US (Uniform Sampling)")
+parser.add_argument('--N', type=int, default=10000, help="The number of synthetic data points (only if the --diag flag is not provided)")
+parser.add_argument('--d', type=int, default=100, help="The dimenion of the synthetic data points (if the --diag flag is provided, this is also the number of synthetic data points")
+parser.add_argument('--diag', action='store_const', default=False, const=True, help="If this flag is provided, uses an axis-aligned diagonal dataset (NxN) instead of the usual random Nxd matrix")
+
+arguments = parser.parse_args()
+tr = arguments.tr
+alg_nm = arguments.alg_nm
+N = arguments.N
+d = arguments.d
+diag = arguments.diag
 
 class IDProjector(bc.Projector):
 
@@ -15,73 +30,56 @@ np.random.seed(3)
 
 bc.util.set_verbosity('error')
 
-n_trials = 5
 Ms = np.unique(np.logspace(0., 4., 100, dtype=np.int32))
 
-anms = ['FW', 'GIGA', 'OMP', 'IS', 'US']
-algs = [bc.snnls.FrankWolfe, bc.snnls.GIGA, bc.snnls.OrthoPursuit, bc.snnls.ImportanceSampling, bc.snnls.UniformSampling]
+algs = {'FW': bc.snnls.FrankWolfe, 
+        'GIGA': bc.snnls.GIGA,
+        'OMP': bc.snnls.OrthoPursuit, 
+        'IS': bc.snnls.ImportanceSampling, 
+        'US': bc.snnls.UniformSampling}
 
 ##########################################
 ## Test 1: gaussian data
 ##########################################
-N = 10000
-D = 100
 
-err = np.zeros((len(anms), n_trials, Ms.shape[0]))
-opt_err = np.zeros((len(anms), n_trials, Ms.shape[0]))
-csize = np.zeros((len(anms), n_trials, Ms.shape[0]))
-cput = np.zeros((len(anms), n_trials, Ms.shape[0]))
-for tr in range(n_trials):
-  X = np.random.randn(N, D)
+#######################################
+#######################################
+## Step 0: Generate a Synthetic Dataset
+#######################################
+#######################################
 
-  for aidx, anm in enumerate(anms):
-    print('data: gauss, trial ' + str(tr+1) + '/' + str(n_trials) + ', alg: ' + anm)
-    alg = bc.HilbertCoreset(X, IDProjector(), snnls = algs[aidx])
+if diag:
+  X = np.eye(d)
+else: 
+  X = np.random.randn(N, d)
 
-    for m, M in enumerate(Ms):
-      t0 = time.time()
-      itrs = (Ms[m] if m == 0 else Ms[m] - Ms[m-1])
-      alg.build(itrs) #no explicit bound on size, just run correct # iterations (size will be upper bounded by # itrs)
-      tf = time.time()
-      cput[aidx, tr, m] = tf-t0 + cput[aidx, tr, m-1] if m > 0 else tf-t0
-      wts, pts, idcs = alg.get()
-      csize[aidx, tr, m] = (wts > 0).sum()
-      err[aidx, tr, m] = alg.error()
+############################
+############################
+## Step 1: Build/Evaluate the Coreset
+############################
+############################
+err = np.zeros(Ms.shape[0])
+opt_err = np.zeros(Ms.shape[0])
+csize = np.zeros(Ms.shape[0])
+cput = np.zeros(Ms.shape[0])
 
-np.savez_compressed('gauss_results.npz', err=err, csize=csize, cput=cput, Ms = Ms, anms=anms)
+print('data: gauss, trial ' + str(tr) + ', alg: ' + alg_nm)
+alg = bc.HilbertCoreset(X, IDProjector(), snnls = algs[alg_nm])
 
-##########################################
-## Test 2: axis-aligned data
-##########################################
- 
-N = 5000
-N = 100
+for m, M in enumerate(Ms):
+  t0 = time.time()
+  itrs = (Ms[m] if m == 0 else Ms[m] - Ms[m-1])
+  alg.build(itrs) #no explicit bound on size, just run correct # iterations (size will be upper bounded by # itrs)
+  tf = time.time()
+  cput[m] = tf-t0 + cput[m-1] if m > 0 else tf-t0
+  wts, pts, idcs = alg.get()
+  csize[m] = (wts > 0).sum()
+  err[m] = alg.error()
 
-X = np.eye(N)
+############################
+############################
+## Step 2: Save Results
+############################
+############################
 
-#create the tangent space factory (in this synthetic vectors example, it's just X)
-def tsf_X():
-    return X
-
-err = np.zeros((len(anms), n_trials, Ms.shape[0]))
-opt_err = np.zeros((len(anms), n_trials, Ms.shape[0]))
-csize = np.zeros((len(anms), n_trials, Ms.shape[0]))
-cput = np.zeros((len(anms), n_trials, Ms.shape[0]))
-for tr in range(n_trials):
-  for aidx, anm in enumerate(anms):
-    print('data: axis, trial ' + str(tr+1) + '/' + str(n_trials) + ', alg: ' + anm)
-    alg = bc.HilbertCoreset(X, IDProjector(), snnls = algs[aidx])
-
-    for m, M in enumerate(Ms):
-      t0 = time.time()
-      itrs = (Ms[m] if m == 0 else Ms[m] - Ms[m-1])
-      alg.build(itrs) #no explicit bound on size, just run correct # iterations (size will be upper bounded by # itrs)
-
-      tf = time.time()
-      cput[aidx, tr, m] = tf-t0 + cput[aidx, tr, m-1] if m > 0 else tf-t0
-      wts, pts, idcs = alg.get()
-      csize[aidx, tr, m] = (wts > 0).sum()
-      err[aidx, tr, m] = alg.error()
-
-np.savez_compressed('axis_results.npz', err=err, csize=csize, cput=cput, Ms = Ms, anms=anms)
-
+np.savez_compressed('gauss_results'+('_diag'if diag else '')+'_alg='+alg_nm+'_tr='+str(tr)+'_N='+str(N)+'_d='+str(d)+'.npz', err=err, csize=csize, cput=cput, Ms = Ms, alg=alg, tr=tr)
