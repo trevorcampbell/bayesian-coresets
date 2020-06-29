165b2e1fa7265d7bbb9f389d955faba351e7438c
HaydenMcT <50335060+HaydenMcT@users.noreply.github.com>
2020-06-29 14:09:39 -0700

    Introducing further command line functionality to plot.py

--- examples/hilbert_logistic_poisson_regression/plot.py
+++ examples/hilbert_logistic_poisson_regression/plot.py
@@ -1,67 +1,92 @@
-import numpy as np
 import bokeh.plotting as bkp
-import bokeh.io as bki
-import bokeh.layouts as bkl
-from bokeh.models import FuncTickFormatter
-import bokeh.models as bkm
-import bokeh.palettes 
-import time
-import os, sys
+from bokeh.io import export_png, export_svgs
+import numpy as np
+import sys, os
+import argparse
+import hashlib
 #make it so we can import models/etc from parent folder
 sys.path.insert(1, os.path.join(sys.path[0], '../common'))
 from plotting import *
 
-dnames = ['synth_poiss']#['synth_lr', 'biketrips', 'airportdelays', 'synth_poiss', 'ds1', 'phishing']
-algs = [('RND', 'Uniform', pal[7]), ('GIGA','GIGA', pal[1])]
+parser = argparse.ArgumentParser(description="Plots Riemannian linear regression experiments")
+parser.add_argument('--X', type = str, default="Iterations", help="The X axis of the plot - one of Iterations/Coreset Size/Forward KL/CPU Time(s)")
+parser.add_argument('--X_scale', type=str, choices=["linear","log"], default = "linear", help = "Specifies the scale for the X-axis. Default is \"linear\".")
+parser.add_argument('--Y', type = str, default = "Forward KL", help="The Y axis of the plot - one of Iterations/Coreset Size/Forward KL/CPU Time(s)")
+parser.add_argument('--Y_scale', type=str, choices=["linear","log"], default = "log", help = "Specifies the scale for the Y-axis. Default is \"log\".")
 
-fig_cput = bkp.figure(y_axis_type='log', y_axis_label='Normalized Fisher Information Distance', x_axis_type='log', x_axis_label='Relative Total CPU Time', x_range=(.05, 1.1), plot_width=1250, plot_height=1250)
-fig_csz = bkp.figure(y_axis_type='log', y_axis_label='Normalized Fisher Information Distance', x_axis_type='log', x_axis_label='Coreset Size', plot_width=1250, plot_height=1250)
+parser.add_argument('--height', type=int, default=850, help = "Height of the plot's html canvas, default 850")
+parser.add_argument('--width', type=int, default=850, help = "Width of the plot's html canvas, default 850")
 
-preprocess_plot(fig_cput, '32pt', False, True)
-preprocess_plot(fig_csz, '32pt', True, True)
+parser.add_argument('dnm', type=str, help="the name of the dataset for which to plot results")
+parser.add_argument('model', type=str, choices=["lr","poiss"], help="The regression model used. lr refers to logistic regression, and poiss refers to poisson regression.")
 
-dnmsalgs = [(dnm, alg) for dnm in dnames for alg in algs]
+parser.add_argument('names', type = str, nargs = '+', default = ["SVI", "RAND", "GIGAO", "GIGAR"], help = "a list of which algorithm names to plot results for (Examples: SVI / GIGAO / GIGAR / RAND)")
+trials = parser.add_mutually_exclusive_group(required=True)
 
-#get RND median normalization
-NMs = -1
-std_Fs = {}
-std_ts = {}
-for didx, dnm in enumerate(dnames):
-  trials = [fn for fn in os.listdir('results/') if dnm+'_RND_results_' in fn]
-  if len(trials) == 0: 
-    print('Need to run RND to establish baseline first')
-    quit()
-  Fs = []
-  for tridx, fn in enumerate(trials):
-    res = np.load('results/'+fn)
-    Fs.append(res['Fs'])
-    NMs = res['Ms'].shape[0]
-  std_Fs[dnm] = np.percentile(np.array(Fs), 50)
-  std_ts[dnm] = np.load('caching/'+dnm+'_mcmc_time.npy')
+trials.add_argument('--n_trials', type=int, help="Look for & plot experiments with trial IDs 1 through n_trials (inclusive)")
+trials.add_argument('--seeds', type = int, nargs = '+', help="Plot experiments associated with the provided trial numbers (seeds)")
 
-for idx, zppd in enumerate(dnmsalgs):
-  dnm, alg = zppd
-  trials = [fn for fn in os.listdir('results/') if dnm+'_'+alg[0]+'_results_' in fn]
-  if len(trials) == 0: continue
-  cputs = np.zeros((len(trials), NMs))
-  cszs = np.zeros((len(trials), NMs))
-  Fs = np.zeros((len(trials), NMs))
-  for tridx, fn in enumerate(trials):
-    res = np.load('results/'+fn)
-    cputs[tridx, :] = res['cputs']
-    Fs[tridx, :] = res['Fs']
-    cszs[tridx, :] = res['csizes']
+parser.add_argument('--fldr', type=str, default="results/", help="This script will look for & plot experiments in this folder")
+parser.add_argument('--proj_dim', type=int, default = 500, help = "The number of samples taken when discretizing log likelihoods for these experiments")
 
-  fig_cput.line(np.percentile(cputs, 50, axis=0)/std_ts[dnm], np.percentile(Fs, 50, axis=0)/std_Fs[dnm], line_color=alg[2], line_width=8, legend=alg[1])
-  fig_csz.line(np.percentile(cszs, 50, axis=0), np.percentile(Fs, 50, axis=0)/std_Fs[dnm], line_color=alg[2], line_width=8, legend=alg[1])
+parser.add_argument("--mcmc_samples_full", type=int, default=10000, help="number of MCMC samples to take for the posterior estimate using the full dataset (we also take this many warmup steps before sampling)")
+parser.add_argument("--mcmc_samples_coreset", type=int, default=10000, help="number of MCMC samples to take for the posterior estimate using our coreset (we also take this many warmup steps before sampling)")
+parser.add_argument("--Ms", type=int, nargs='+', default = None, help = "A list of M values (maximum allowable coreset sizes) to try in this experiment.")
+parser.add_argument("--num_Ms", type=int, default=10, help="(if --Ms is not specified) The number of different coreset sizes (Ms) to try in this experiment (We try Ms from 1 to --M_max, inclusive, with logarithmic spacing between values of M). Default 10.")
+parser.add_argument("--M_max", type=int, default=1000, help="(if --Ms is not specified) The largest coreset size to try in this experiment. Default 1000.")
 
-rndlbl = bkm.Label(x=1.0, x_offset=-10, y=700, y_units='screen', text='Full Dataset MCMC', angle=90, angle_units='deg', text_font_size='30pt')
-rndspan = bkm.Span(location = 1.0, dimension='height', line_width=8, line_color='black', line_dash='40 40')
-fig_cput.add_layout(rndspan)
-fig_cput.add_layout(rndlbl)
+arguments = parser.parse_args()
+X = arguments.X
+X_scale = arguments.X_scale
+Y = arguments.Y
+Y_scale = arguments.Y_scale
+height = arguments.height
+width = arguments.width
 
-postprocess_plot(fig_cput, '22pt', location='bottom_left', glyph_width=40)
-postprocess_plot(fig_csz, '22pt', location='bottom_left', glyph_width=40)
+names = arguments.names
+trials = np.arange(1, arguments.n_trials + 1) if arguments.n_trials else arguments.seeds
+model = arguments.model
+dnm = arguments.dnm
+fldr = arguments.fldr
 
-bkp.show(bkl.gridplot([[fig_cput, fig_csz]]))
+mcmc_samples_full = arguments.mcmc_samples_full
+mcmc_samples_coreset = arguments.mcmc_samples_coreset
+projection_dim = arguments.proj_dim
 
+Ms = arguments.Ms if arguments.Ms is not None else np.unique(np.logspace(0, np.log10(arguments.M_max), arguments.num_Ms, dtype=int))
+
+algs = {'SVIEXACT': 'Sparse VI (Exact Tangent Space)',
+        'SVI': 'Sparse VI', 
+        'GIGAO': 'GIGA(Optimal)', 
+        'GIGAR': "GIGA(Realistic)", 
+        'RAND': "Uniform"}
+nms = []
+for name in names:
+  nms.append((name, algs[name]))
+
+#plot the KL figure
+fig = bkp.figure(y_axis_type=Y_scale, x_axis_type=X_scale, plot_width=width, plot_height=height, x_axis_label=X, y_axis_label=Y, toolbar_location=None )
+preprocess_plot(fig, '32pt', X_scale == 'log', Y_scale == 'log')
+
+for i, nm in enumerate(nms):
+  kl = []
+  sz = []
+  for tr in trials:
+    numTuple = (dnm, model, nm[0], "results", "id="+str(tr), "mcmc_samples_coreset="+str(mcmc_samples_coreset), "mcmc_samples_full="+str(mcmc_samples_full), "proj_dim="+str(proj_dim), 'Ms='+str(Ms))
+    print(os.path.join(fldr, '_'.join(numTuple)+'.npz'))
+    res = np.load(os.path.join(fldr, '_'.join(numTuple)+'.npz'), allow_pickle = True)
+    data = { 'Iterations': res['Ms'],
+             'Coreset Size': res['csizes'],
+             'Forward KL': res['Fs']
+             'CPU Time(s)': res['cputs']}
+             
+  x = np.percentile(data[X], 50, axis=0)
+  fig.line(x, np.percentile(data[Y], 50, axis=0), color=pal[i-1], line_width=5, legend=nm[1])
+  fig.patch(x = np.hstack((x, x[::-1])), y = np.hstack((np.percentile(data[Y], 75, axis=0), np.percentile(data[Y], 25, axis=0)[::-1])), color=pal[i-1], fill_alpha=0.4, legend=nm[1])
+
+postprocess_plot(fig, '12pt', location='bottom_left', glyph_width=40)
+fig.legend.background_fill_alpha=0.
+fig.legend.border_line_alpha=0.
+fig.legend.visible = True
+
+bkp.show(fig)
