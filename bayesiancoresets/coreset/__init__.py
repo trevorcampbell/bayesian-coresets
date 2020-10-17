62ee991507346fe7596d34d2eeda71a5d10bdd1e
Trevor Campbell <trevor.d.campbell@gmail.com>
2020-10-17 12:08:58 -0700

    starting to clean for merge post summer 2020. This will be the main branch, tcpseudo and pseudo will merge into this before going public master

--- bayesiancoresets/coreset/__init__.py
+++ bayesiancoresets/coreset/__init__.py
@@ -2,5 +2,4 @@ from .hilbert import HilbertCoreset
 from .sampling import UniformSamplingCoreset
 from .sparsevi import SparseVICoreset
 from .bpsvi import BatchPSVICoreset
-from .hops import HOPSCoreset
 
--- bayesiancoresets/snnls/__init__.py
+++ bayesiancoresets/snnls/__init__.py
@@ -2,4 +2,3 @@ from .frankwolfe import FrankWolfe
 from .sampling import ImportanceSampling, UniformSampling
 from .giga import GIGA
 from .orthopursuit import OrthoPursuit
-#from .lar import LAR
