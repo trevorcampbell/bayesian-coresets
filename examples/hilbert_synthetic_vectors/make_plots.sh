2dbe0531523b73c22fba44176a11b3c290c98282
Trevor Campbell <trevor.d.campbell@gmail.com>
2020-10-17 19:11:47 -0700

    removed comments

--- examples/hilbert_synthetic_vectors/make_plots.sh
+++ examples/hilbert_synthetic_vectors/make_plots.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-#python3 main.py plot Ms err --plot_legend alg_nm --summarize trial
 python3 main.py plot Ms err --summarize trial --groupby Ms --plot_toolbar --plot_type line --plot_legend alg_nm --plot_x_label "Iterations" --plot_y_label Error
 
-#python3 main.py plot csize err --plot_legend alg_nm --summarize trial
 python3 main.py plot csize err --summarize trial --groupby Ms --plot_type line --plot_legend alg_nm --plot_x_label "Coreset Size" --plot_y_label Error
 
-#python3 main.py plot cput err --plot_legend alg_nm --summarize trial
 python3 main.py plot cput err --summarize trial --groupby Ms --plot_type line --plot_legend alg_nm --plot_x_label "CPU Time" --plot_y_label Error
