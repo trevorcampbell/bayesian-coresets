ca00885169fba79e06612d0b178e6a6435c03189
Trevor Campbell <trevor.d.campbell@gmail.com>
2020-10-17 21:19:43 -0700

    moved stan test code into mcmc

--- examples/common/mcmc.py
+++ examples/common/mcmc.py
@@ -95,3 +95,52 @@ def load_modified_cpp_code(code_folder, model_name, model_code):
     file.close()
     raise EnvironmentError("No modified code to handle weighted data present - unable to use stan for MCMC sampling. Please modify the file "+str(path_to_file_to_find)+" to handle weighted data. See the ReadMe for more information.")
 
+
+# TODO test weighted CPP using randomly chosen integer weights
+#arguments = parser.parse_args()
+#dnm = arguments.dnm
+#ID = arguments.ID
+#model = arguments.model
+#numIdcs = arguments.numIdcs
+#mcmc_samples = arguments.mcmc_samples
+#verbose_compiling = arguments.verbose_compiling
+#
+##lrdnms = ['synth_lr', 'phishing', 'ds1', 'synth_lr_large', 'phishing_large', 'ds1_large']
+##prdnms = ['synth_poiss', 'biketrips', 'airportdelays', 'synth_poiss_large', 'biketrips_large', 'airportdelays_large']
+#if model=="lr":
+#  from model_lr import *
+#elif model=="poiss":
+#  from model_poiss import *
+#
+#np.random.seed(ID)
+#
+#print('Loading dataset '+dnm)
+#X,Y,Z, Zt, D = load_data('../data/'+dnm+'.npz')
+#print("N, d: "+str(X.shape))
+#
+#idcs = np.random.choice(X.shape[0],numIdcs, replace=True) #random.choice is a bit slower without replacement, but may still be worth trying
+#weights = np.random.randint(1,10,numIdcs)
+#curX = X[idcs, :]
+#curY = Y[idcs]
+##run the weighted version of sampler
+## (we want to make sure we use a model based on the current cpp code for weighted coresets, so we don't use any cached models)
+#samples_using_code_for_weights = mcmc.sampler(dnm, curX, curY, mcmc_samples, stan_representation, weights=weights, model_caching_folder = None, verbose_compiling = verbose_compiling, seed = ID) 
+## build the modified dataset so that we can run the unweighted version of sampler
+#for i in range(len(idcs)):
+#    toAdd = np.ones(weights[i]-1,dtype=int) * (i) #list i a number of times equal to the weight of the ith index
+#    curX = np.append(curX, curX[toAdd], axis=0)
+#    curY = np.append(curY, curY[toAdd])
+#samples_using_standard_stan_code = mcmc.sampler(dnm, curX, curY, mcmc_samples, stan_representation, seed = ID)
+#
+#relative_err = np.abs(np.linalg.norm(samples_using_code_for_weights - samples_using_standard_stan_code, axis=1)/np.linalg.norm(samples_using_standard_stan_code,axis=1))
+#print("largest relative error between samples using code for weights and samples using standard stan code (should be 0): ")
+#print(np.max(relative_err))
+#print("average relative error")
+#print(np.mean(relative_err))
+#print("median relative error")
+#print(np.quantile(relative_err,.5))
+#print("first quartile relative error")
+#print(np.quantile(relative_err,.25))
+#print("third quartile relative error")
+#print(np.quantile(relative_err,.75))
+
