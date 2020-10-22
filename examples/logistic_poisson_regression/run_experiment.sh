8105f5d450e6c6cc7a219720b4417e005639ef8c
Trevor Campbell <trevor.d.campbell@gmail.com>
2020-10-22 00:40:38 -0700

    rearranged run expt loops in log/poiss

--- examples/logistic_poisson_regression/run_experiment.sh
+++ examples/logistic_poisson_regression/run_experiment.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
-for ID in {1..3}
+for dnm in "synth_lr" "phishing" "ds1"
 do
     for alg in "US" "GIGA-OPT" "GIGA-REAL" "SVI" 
     do
-        for dnm in "synth_lr" "phishing" "ds1"
-	do
+        for ID in {1..3}
+        do
 		python3 main.py --model lr --dataset $dnm --alg $alg --trial $ID run
 	done
     done
 done
 
-#for ID in {1..3}
-#do
-#    for alg in "US" "FW" "GIGA" 
-#    do
-#        for dnm in "synth_poiss" "biketrips" "airportdelays"
-#	do
-#		python3 main.py --model poiss --dataset $dnm --alg $alg --trial $ID run 
-#	done
-#    done
-#done
-#
 
-
- 
